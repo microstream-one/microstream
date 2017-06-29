bb7c85e07f1cdbdee8bcfd6afa739656a1de3f2a
TM <t.muenz@xdev-software.de>
2017-06-29 14:09:50 +0200

    Multithreading fix in JadothFiles#ensureDirectory

--- net.jadoth.test.core/src/net/jadoth/util/MainSearchStringInFiles.java
+++ net.jadoth.test.core/src/net/jadoth/util/MainSearchStringInFiles.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.util.function.Consumer;
 
 import net.jadoth.functional.BiProcedure;
+import net.jadoth.functional.TriConsumer;
 import net.jadoth.util.chars.VarString;
 import net.jadoth.util.file.JadothFiles;
 
@@ -21,12 +22,41 @@ public class MainSearchStringInFiles
 
 	public static void main(final String[] args) throws Exception
 	{
-		// 1000000000049275254 ist das erste KonzAbschlagAnspruchAhVlUSt von KonzAbschlagAnsprueche hashTable 1000000000049005596
 		searchStringsInFiles(DIRECT,
-			new File("D:/BonusExportTest_2017-06-09_15-03-44.925/csv").listFiles(),
-			"1000000000024649203"
+			new File("C:/BonusExportTest_2017-06-27_15-36-07.196/csv").listFiles(),
+			MainSearchStringInFiles::printOid,
+//			"1000000000025679576" // 0 ZahlungAusschuettungbetrag$Implementation
+//			"1000000000025680038" // 1 = 0 referenzierende ZahlungAuszahlung$Implementation
+//			"1000000000025679571" // 2 = 0 referenzierende HashTable
+			"1000000000025679567" // 4 = 2 referenzierende Lazy
+			
+			// KonzAusAnspruchAhg
+//			"1000000000039543476" // 0 KonzAusAnspruchAhg$Implementation
+//			"1000000000039546071" // 1 = 0 referenzierende ZahlungAusschuettungbetrag$Implementation
+//			"1000000000039546799" // 2 = 1 referenzierende ZahlungAuszahlung$Implementation
+//			"1000000000039546067" // 3 = 1 referenzierende HashTable
+//			"1000000000039546063" // 5 = 3 referenzierende Lazy
+//			"1000000000039530216" // 7 = 5 referenzierende VorgangKonzAusschuettungNachStaffel
+			
+			// HausAusAnspruchAhg
+//			"1000000000025153392" // 0 HausAusAnspruchAhg$Implementation
+//			"1000000000025155744" // 1 = 0 referenzierende ZahlungAusschuettungbetrag$Implementation
+//			"1000000000025155738" // 2 = 1 referenzierende HashTable
+//			"1000000000025155897" // 3 = 1 referenzierende ZahlungAuszahlung$Implementation
+//			"1000000000025155734" // 4 = 2 referenzierende Lazy (#ausschuettungsbetraege)
+//			"1000000000025155740" // 5 = 3 referenzierende HashTable
+//			"1000000000025150620" // 6 = 4 referenzierende VorgangHausAusschuettung
+//			"1000000000025155736" // 7 = 5 referenzierende Lazy (#auszahlungen)
+//			"1000000000025150620" // 6 = 7 referenzierende VorgangHausAusschuettung
 		);
-
+	}
+	
+	static void printOid(final String s, final Integer index, final Integer endIndex)
+	{
+		final int newLineIndex = s.lastIndexOf('\n', index);
+		// 20 ist die Länge einer OID
+		System.out.println("Line: " + s.substring(newLineIndex + 1, newLineIndex + 1 + 20));
+//		System.out.println("Line: " + s.substring(newLineIndex + 1, endIndex));
 	}
 
 
@@ -58,30 +88,42 @@ public class MainSearchStringInFiles
 
 	static void searchStringsInFiles(final File[] files, final String... strings)
 	{
-		searchStringsInFiles(DIRECT, files, strings);
+		searchStringsInFiles(files, null, strings);
+	}
+	
+	static void searchStringsInFiles(
+		final File[]                                files        ,
+		final TriConsumer<String, Integer, Integer> matchCallback,
+		final String...                             strings
+	)
+	{
+		searchStringsInFiles(DIRECT, files, matchCallback, strings);
 	}
 
 	static void searchStringsInFiles(
-		final BiProcedure<File, Consumer<? super File>> logic  ,
-		final File[]                                     files  ,
-		final String...                                  strings
+		final BiProcedure<File, Consumer<? super File>> logic        ,
+		final File[]                                    files        ,
+		final TriConsumer<String, Integer, Integer>     matchCallback,
+		final String...                                 strings
 	)
 	{
 		final long tStart = System.nanoTime();
 		for(final File f : files)
 		{
-			logic.accept(f, file -> searchStringsInFile(file, strings));
+			logic.accept(f, file -> searchStringsInFile(file, matchCallback, strings));
 			System.gc();
 		}
 		final long tStop = System.nanoTime();
 		System.out.println("Elapsed Time: " + new java.text.DecimalFormat("00,000,000,000").format(tStop - tStart));
 	}
 
-	static void searchStringsInFile(final File f, final String... strings)
+	static void searchStringsInFile(
+		final File                                  f            ,
+		final TriConsumer<String, Integer, Integer> matchCallback,
+		final String...                             strings
+	)
 		throws RuntimeException
 	{
-
-
 		try
 		{
 			final String fileContent = JadothFiles.readStringFromFile(f);
@@ -90,7 +132,11 @@ public class MainSearchStringInFiles
 				final int index = fileContent.indexOf(s);
 				if(index >= 0)
 				{
-					System.out.println(index+"@"+f+" is "+s);
+					System.out.println(index + "@" + f + " is " + s);
+					if(matchCallback != null)
+					{
+						matchCallback.accept(fileContent, index, index + s.length());
+					}
 				}
 			}
 		}
