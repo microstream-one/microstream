9634ab9e2e0136d85f227be9639466f5701c42b5
TM <t.muenz@xdev-software.de>
2018-09-13 17:43:03 +0200

    OGS-3: Field refactoring mapping implementation. Testing.

--- net.jadoth.test.storage/src/net/jadoth/test/legacy/MainTestStorageLegacyMapping.java
+++ net.jadoth.test.storage/src/net/jadoth/test/legacy/MainTestStorageLegacyMapping.java
@@ -1,6 +1,9 @@
 package net.jadoth.test.legacy;
 
+import java.io.File;
+
 import net.jadoth.X;
+import net.jadoth.persistence.types.Persistence;
 import net.jadoth.storage.types.EmbeddedStorage;
 import net.jadoth.storage.types.EmbeddedStorageManager;
 import net.jadoth.test.corp.logic.Test;
@@ -8,7 +11,13 @@ import net.jadoth.test.corp.logic.Test;
 public class MainTestStorageLegacyMapping
 {
 	// create a storage manager, link the root, start the "embedded" database
-	static final EmbeddedStorageManager STORAGE = EmbeddedStorage.start();
+	static final EmbeddedStorageManager STORAGE = EmbeddedStorage
+		.createFoundation()
+		.setRefactoringMappingProvider(
+			Persistence.RefactoringMapping(new File("D:/Refactorings.csv"))
+		)
+		.start()
+	;
 
 	public static void main(final String[] args)
 	{
