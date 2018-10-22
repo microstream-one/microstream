b2d7a24f0ce9c0d9d5f3cd7719a0281fa4799f53
TMuenz <t.muenz@xdev-software.de>
2018-10-23 00:30:50 +0200

    OGS-45: Implementation.

---
+++ net.jadoth.storage/src/net/jadoth/storage/io/fs/ProtageFileSystem.java
@@ -0,0 +1,68 @@
+package net.jadoth.storage.io.fs;
+
+import java.io.File;
+import java.io.RandomAccessFile;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
+
+import net.jadoth.files.XFiles;
+
+public final class ProtageFileSystem
+{
+	public static final File createWriteableFile(final File directory, final String fileName)
+	{
+		final File file = new File(directory, fileName);
+		XFiles.ensureWriteableFile(file);
+		
+		return file;
+	}
+	
+	@SuppressWarnings("resource") // resource closed internally by FileChannel (JDK tricking Java compiler ^^)
+	public static FileLock openFileChannel(final File file)
+	{
+		// the file is always completely and unshared locked.
+		final FileLock lock;
+		FileChannel channel = null;
+		try
+		{
+			// resource closed internally by FileChannel (JDK tricking Java compiler ^^)
+			channel = new RandomAccessFile(file, "rw").getChannel();
+
+			/*
+			 * Tests showed that Java file locks even on Windows don't work properly:
+			 * They only prevent other Java processes from acquiring another lock.
+			 * But other applications (e.g. Hexeditor) can still open and write to the file.
+			 * This basically makes any attempt to secure the file useless.
+			 * Not only on linux which seems to be complete crap when it comes to locking files,
+			 * but also on windows.
+			 * As there is no alternative available and it at least works within Java, it is kept nevertheless.
+			 */
+			lock = channel.tryLock();
+			if(lock == null)
+			{
+				throw new RuntimeException("File seems to be already locked: " + file);
+			}
+		}
+		catch(final Exception e)
+		{
+			XFiles.closeSilent(channel);
+			// (28.06.2014)EXCP: proper exception
+			throw new RuntimeException("Cannot obtain lock for file " + file, e);
+		}
+
+		return lock;
+	}
+	
+	
+
+	///////////////////////////////////////////////////////////////////////////
+	// constructors //
+	/////////////////
+
+	private ProtageFileSystem()
+	{
+		// static only
+		throw new UnsupportedOperationException();
+	}
+	
+}
