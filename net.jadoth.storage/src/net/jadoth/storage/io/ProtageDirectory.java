63ed942269dd1358fc16bc4501bd6df4e2093dc4
TMuenz <t.muenz@xdev-software.de>
2018-10-27 23:39:06 +0200

    OGS-45: Implementation.

--- net.jadoth.storage/src/net/jadoth/storage/io/ProtageDirectory.java
+++ net.jadoth.storage/src/net/jadoth/storage/io/ProtageDirectory.java
@@ -1,5 +1,7 @@
 package net.jadoth.storage.io;
 
+import java.util.function.Supplier;
+
 import net.jadoth.collections.types.XGettingTable;
 
 public interface ProtageDirectory
@@ -43,4 +45,43 @@ public interface ProtageDirectory
 	}
 	
 	public boolean contains(String fileName);
+	
+	
+	/**
+	 * A simple deadlock prevention strategy:<br>
+	 * - directories are locked before files
+	 * - directories with higher identifiers are locked before those with lower identifier
+	 * - files with higher identifiers are locked before those with lower identifier
+	 * 
+	 * @param directory1
+	 * @param directory2
+	 * @param logic
+	 */
+	public static ProtageWritableFile executeLocked(
+		final ProtageWritableDirectory      directory1,
+		final ProtageWritableDirectory      directory2,
+		final Supplier<ProtageWritableFile> logic
+	)
+	{
+		synchronized(directory1)
+		{
+			// deadlock-prevention strategy
+			if(directory1.identifier().compareTo(directory2.identifier()) >= 0)
+			{
+				synchronized(directory2)
+				{
+					return logic.get();
+				}
+			}
+		}
+		
+		synchronized(directory2)
+		{
+			synchronized(directory1)
+			{
+				return logic.get();
+			}
+		}
+	}
+	
 }
