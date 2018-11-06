cd62bdb038772228d0824f9a2401975eda8a9f00
TM <t.muenz@xdev-software.de>
2018-11-06 13:34:45 +0100

    JET-43: Implementation and testing.

--- net.jadoth.network.persistence.binary/src/net/jadoth/network/persistence/binary/MainTestParseProtocol.java
+++ net.jadoth.network.persistence.binary/src/net/jadoth/network/persistence/binary/MainTestParseProtocol.java
@@ -1,10 +1,13 @@
 package net.jadoth.network.persistence.binary;
 
+import java.io.File;
+
 import net.jadoth.network.persistence.Com;
 import net.jadoth.network.persistence.ComFoundation;
 import net.jadoth.network.persistence.ComProtocol;
 import net.jadoth.network.persistence.ComProtocolStringConverter;
 import net.jadoth.persistence.binary.types.BinaryPersistence;
+import net.jadoth.persistence.internal.PersistenceTypeDictionaryFileHandler;
 import net.jadoth.persistence.types.PersistenceTypeDictionary;
 
 public class MainTestParseProtocol
@@ -12,6 +15,7 @@ public class MainTestParseProtocol
 	public static void main(final String[] args)
 	{
 		final PersistenceTypeDictionary td = BinaryPersistence.foundation()
+			.setTypeDictionaryIoHandler(PersistenceTypeDictionaryFileHandler.NewInDirecoty(new File("TypeDictionary")))
 			.getTypeDictionaryManager()
 			.provideTypeDictionary()
 		;
@@ -23,6 +27,9 @@ public class MainTestParseProtocol
 		final ComProtocol                protocol   = foundation.getProtocol();
 		final ComProtocolStringConverter converter  = foundation.getProtocolStringConverter();
 		final String                     assembled  = converter.assemble(protocol);
+		
+		System.out.println(assembled);
+		
 		final ComProtocol                parsed     = converter.parse(assembled);
 		System.out.println(converter.assemble(parsed));
 	}
