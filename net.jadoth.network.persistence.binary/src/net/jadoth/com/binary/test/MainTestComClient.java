8c6afb1fc011e7fe692e0789f702705640305bb3
TMuenz <t.muenz@xdev-software.de>
2018-11-19 00:19:54 +0100

    JET-44: Planning, implementation and testing.

--- net.jadoth.network.persistence.binary/src/net/jadoth/com/binary/test/MainTestComClient.java
+++ net.jadoth.network.persistence.binary/src/net/jadoth/com/binary/test/MainTestComClient.java
@@ -1,19 +1,21 @@
 package net.jadoth.com.binary.test;
 
-import net.jadoth.com.Com;
 import net.jadoth.com.ComChannel;
-import net.jadoth.com.binary.ComPersistenceAdaptorBinary;
+import net.jadoth.com.binary.ComBinary;
 
 public class MainTestComClient
 {
-	// (16.11.2018 TM)TODO: Convenience client methods
-	// (18.11.2018 TM)TODO: set localhost as default address plus an arbitrary default port
-	static final ComChannel COM = Com.Foundation()
-//		.setClientTargetAddress(XSockets.localHostSocketAddress(1337))
-		.setPersistenceAdaptor(ComPersistenceAdaptorBinary.New())
-		.createClient()
-		.connect()
-	;
+	
+//	static final ComChannel COM = Com.Foundation()
+////		.setClientTargetAddress(XSockets.localHostSocketAddress(1337))
+//		.setPersistenceAdaptor(ComPersistenceAdaptorBinary.New())
+//		.createClient()
+//		.connect()
+//	;
+
+//	static final ComChannel COM = Com.connect(ComPersistenceAdaptorBinary.New());
+	
+	static final ComChannel COM = ComBinary.connect();
 	
 	public static void main(final String[] args)
 	{
