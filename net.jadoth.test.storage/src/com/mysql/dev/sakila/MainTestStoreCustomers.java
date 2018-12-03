4fa94351acada56646a04f78d26435ddc6911b7f
TMuenz <t.muenz@xdev-software.de>
2018-12-03 16:13:07 +0100

    Sakila test code.

--- net.jadoth.test.storage/src/com/mysql/dev/sakila/MainTestStoreCustomers.java
+++ net.jadoth.test.storage/src/com/mysql/dev/sakila/MainTestStoreCustomers.java
@@ -1,6 +1,7 @@
 package com.mysql.dev.sakila;
 
 import java.io.File;
+import java.util.ArrayList;
 
 import net.jadoth.reference.Reference;
 import net.jadoth.storage.types.EmbeddedStorage;
@@ -36,14 +37,43 @@ public class MainTestStoreCustomers
 		.start()
 	;
 	// (03.12.2018 TM)FIXME: set root
-	
+
+	private static final int RUNS  = 1000;
+	private static final int COUNT = 1_000_000;
 	
 	
 	public static void main(final String[] args)
 	{
-		throw new net.jadoth.meta.NotImplementedYetError(); // FIXME MainTestStoreCustomers#main()
+		final ArrayList<Customer> customers = generateEntities();
+		
+		for(int r = 1; r <= RUNS; r++)
+		{
+			long tStart, tStop;
+			resetTest();
+			tStart = System.nanoTime();
+			// (03.12.2018 TM)FIXME: store root
+			tStop = System.nanoTime();
+			System.out.println(
+				"#" + r
+				+ " Elapsed Time: " + new java.text.DecimalFormat("00,000,000,000").format(tStop - tStart)
+				+ " (" + (tStop - tStart) / COUNT + " per instance)"
+			);
+			System.gc();
+		}
 	}
 	
-	// (03.12.2018 TM)FIXME: generate customer instances
+	static ArrayList<Customer> generateEntities()
+	{
+		// (03.12.2018 TM)FIXME: generate customer instances
+		throw new net.jadoth.meta.NotImplementedYetError();
+	}
+	
+	static void resetTest()
+	{
+		// the central object registry holding the internal object<->objectId associations must be reset
+		STORAGE.persistenceManager().objectRegistry().truncate();
+		System.gc();
+	}
+	
 
 }
