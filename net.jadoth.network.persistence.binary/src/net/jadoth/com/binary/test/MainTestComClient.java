bc6ecc5e55d4d32dcf07e76f4a97bc07daf3f233
TM <t.muenz@xdev-software.de>
2019-01-14 17:07:21 +0100

    JET-49: Customer example and minor code formatting cleanup.

--- net.jadoth.network.persistence.binary/src/net/jadoth/com/binary/test/MainTestComClient.java
+++ net.jadoth.network.persistence.binary/src/net/jadoth/com/binary/test/MainTestComClient.java
@@ -1,6 +1,7 @@
 package net.jadoth.com.binary.test;
 
 import net.jadoth.com.ComChannel;
+import net.jadoth.com.ComClient;
 import net.jadoth.com.binary.ComBinary;
 
 public class MainTestComClient
@@ -24,8 +25,24 @@ public class MainTestComClient
 //		final ComChannel com = Com.connect(ComPersistenceAdaptorBinary.New());
 
 		// convenience & customization example 4
-		final ComChannel com = ComBinary.connect();
+//		final ComChannel com = ComBinary.connect();
+//
+//		System.out.println("Server reply: " + com.request("Hello Server!"));
 		
-		System.out.println("Server reply: " + com.request("Hello Server!"));
+		
+
+		// convenience & customization example 5
+		
+		// setup a client instance for a custom address
+		final ComClient<?> client = ComBinary.Foundation()
+//			.setClientTargetAddress(new InetSocketAddress("www.myAddress.com", 1337))
+			.createClient()
+		;
+		
+		// create a channel by connecting the client
+		final ComChannel channel = client.connect();
+		
+		// send an object graph (customer and its name) through the channel and print the response
+		System.out.println("Server reply: " + channel.request(new Customer("John Doe")));
 	}
 }
