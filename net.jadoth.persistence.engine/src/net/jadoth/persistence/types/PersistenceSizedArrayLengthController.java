8553a1cc164df21c8849ac709b3f04393d8e8ebe
TM <t.muenz@xdev-software.de>
2019-01-24 12:15:20 +0100

    JET-63: PersistenceSizedArrayLengthController implementation.

---
+++ net.jadoth.persistence.engine/src/net/jadoth/persistence/types/PersistenceSizedArrayLengthController.java
@@ -0,0 +1,107 @@
+package net.jadoth.persistence.types;
+
+import net.jadoth.math.XMath;
+
+public interface PersistenceSizedArrayLengthController
+{
+	public int controlArrayLength(int specifiedCapacity, int actualElementCount);
+	
+	
+	
+	/**
+	 * Recommended for storing data (does not change program behavior).
+	 * 
+	 * @return
+	 */
+	public static PersistenceSizedArrayLengthController.Unrestricted Unrestricted()
+	{
+		return new PersistenceSizedArrayLengthController.Unrestricted();
+	}
+	
+	/**
+	 * Recommended for communication (prevents array bombs).
+	 * 
+	 * @return
+	 */
+	public static PersistenceSizedArrayLengthController.Fitting Fitting()
+	{
+		return new PersistenceSizedArrayLengthController.Fitting();
+	}
+	
+	public static PersistenceSizedArrayLengthController.Limited Limited(final int maximumCapacity)
+	{
+		return new PersistenceSizedArrayLengthController.Limited(
+			XMath.positive(maximumCapacity)
+		);
+	}
+	
+	public final class Unrestricted implements PersistenceSizedArrayLengthController
+	{
+		Unrestricted()
+		{
+			super();
+		}
+
+		@Override
+		public final int controlArrayLength(final int specifiedCapacity, final int actualElementCount)
+		{
+			return specifiedCapacity;
+		}
+		
+	}
+	
+	public final class Fitting implements PersistenceSizedArrayLengthController
+	{
+		Fitting()
+		{
+			super();
+		}
+
+		@Override
+		public final int controlArrayLength(final int specifiedCapacity, final int actualElementCount)
+		{
+			return actualElementCount;
+		}
+		
+	}
+	
+	public final class Limited implements PersistenceSizedArrayLengthController
+	{
+		///////////////////////////////////////////////////////////////////////////
+		// instance fields //
+		////////////////////
+		
+		private final int limit;
+		
+		
+		
+		///////////////////////////////////////////////////////////////////////////
+		// constructors //
+		/////////////////
+
+		Limited(final int limit)
+		{
+			super();
+			this.limit = limit;
+		}
+		
+		
+		
+		///////////////////////////////////////////////////////////////////////////
+		// methods //
+		////////////
+		
+		public final int limit()
+		{
+			return this.limit;
+		}
+
+		@Override
+		public final int controlArrayLength(final int specifiedCapacity, final int actualElementCount)
+		{
+			return Math.min(specifiedCapacity, Math.max(this.limit, actualElementCount));
+		}
+		
+	}
+	
+}
