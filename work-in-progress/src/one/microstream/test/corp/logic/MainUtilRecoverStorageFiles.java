de102e225ccaa7d2edbd8790515ca2b8b2f6cc6f
TM <t.muenz@xdev-software.de>
2019-03-14 19:36:58 +0100

    MS-116 Rollback implementation and testing.

--- work-in-progress/src/one/microstream/test/corp/logic/MainUtilRecoverStorageFiles.java
+++ work-in-progress/src/one/microstream/test/corp/logic/MainUtilRecoverStorageFiles.java
@@ -1,8 +1,13 @@
 package one.microstream.test.corp.logic;
 
+import static one.microstream.X.KeyValue;
+
 import java.io.File;
 
 import one.microstream.collections.BulkList;
+import one.microstream.collections.EqHashTable;
+import one.microstream.files.XFiles;
+import one.microstream.meta.XDebug;
 import one.microstream.persistence.binary.types.BinaryEntityRawDataIterator;
 import one.microstream.persistence.types.Persistence;
 import one.microstream.storage.types.StorageDataFileValidator;
@@ -19,14 +24,15 @@ public class MainUtilRecoverStorageFiles
 	;
 	
 	
-	public static void main(final String[] args)
+	public static void main(final String[] args) throws Exception
 	{
-		validateDataFiles();
+		rollbackTransfers();
+//		validateDataFiles();
 //		printTransactionsFile();
 	}
 	
 
-	private static void validateDataFiles()
+	static void validateDataFiles()
 	{
 		final StorageFileProvider sfp = StorageFileProvider
 			.Builder()
@@ -53,12 +59,30 @@ public class MainUtilRecoverStorageFiles
 		}
 	}
 	
-	private static void printTransactionsFile()
+	static void printTransactionsFile()
 	{
 		final StorageTransactionsFile tf = StorageTransactionsFile.parseFile(
 			new File(PATH_CORRUPTED + "/channel_0/transactions_0.sft")
 		);
 		tf.entries().iterate(System.out::println);
+	}
+	
+	static void rollbackTransfers() throws Exception
+	{
+		final File sourceFile = new File(PATH_CORRUPTED + "/channel_0/channel_0_872.dat");
+				
+		final File dir = XFiles.ensureDirectory(new File("D:/_Allianz/20190313_2330_Rollback"));
+		XDebug.deleteAllFiles(dir, false);
+		
+		final StorageTransactionsFile tf = StorageTransactionsFile.parseFile(
+			new File(PATH_CORRUPTED + "/channel_0/transactions_0.sft")
+		);
+		
+		final StorageRollbacker sr = new StorageRollbacker(
+			EqHashTable.New(KeyValue(872L, sourceFile)), dir, dir, "rolledBack_", "storesOnly_"
+		);
+		
+		sr.rollbackTransfers(tf, 863, 607_914);
 	}
 	
 }
