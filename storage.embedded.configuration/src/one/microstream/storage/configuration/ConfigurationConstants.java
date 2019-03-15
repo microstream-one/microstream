7b9a1cb6ac1497d5716339ec65047dcefed578d5
RapidClipse <info@rapidclipse.com>
2019-03-15 11:11:07 +0100

    MS-119 Implementation

---
+++ storage.embedded.configuration/src/one/microstream/storage/configuration/ConfigurationConstants.java
@@ -0,0 +1,22 @@
+
+package one.microstream.storage.configuration;
+
+
+public interface ConfigurationConstants 
+{
+	public final static String	STORAGE_DIRECTORY							= "storage.directory";
+	public final static String	STORAGE_CHANNEL_COUNT						= "storage.channelCount";
+	public final static String	STORAGE_CHANNEL_DIRECTORY_BASENAME			= "storage.channelDirectoryBaseName";
+	public final static String	STORAGE_FILE_BASENAME						= "storage.fileBaseName";
+	public final static String	STORAGE_FILE_SUFFIX							= "storage.fileSuffix";
+	public final static String	STORAGE_FILENAME_TYPE_DICTIONARY			= "storage.filenameTypeDictionary";
+	public final static String	STORAGE_FILENAME_TYPE_ID					= "storage.filenameTypeId";
+	public final static String	STORAGE_FILENAME_OBJECT_ID					= "storage.filenameObjectId";
+	public final static String	STORAGE_HOUSEKEEPING_INTERVAL				= "storage.houseKeeping.interval";
+	public final static String	STORAGE_HOUSEKEEPING_NANO_TIME_BUDGET		= "storage.houseKeeping.nanoTimeBudget";
+	public final static String	STORAGE_ENTITY_CACHE_EVALUATOR_THRESHOLD	= "storage.entityCacheEvaluator.threshold";
+	public final static String	STORAGE_ENTITY_CACHE_EVALUATOR_TIMEOUT		= "storage.entityCacheEvaluator.timeout";
+	public final static String	STORAGE_DATA_FILE_EVALUATOR_MIN_FILE_SIZE	= "storage.dataFileEvaluator.minFileSize";
+	public final static String	STORAGE_DATA_FILE_EVALUATOR_MAX_FILE_SIZE	= "storage.dataFileEvaluator.maxFileSize";
+	public final static String	STORAGE_DATA_FILE_EVALUATOR_DISSOLVE_RATIO	= "storage.dataFileEvaluator.dissolveRatio";
+}
---
+++ storage.embedded.configuration/src/one/microstream/storage/configuration/ConfigurationPropertyParser.java
@@ -0,0 +1,233 @@
+
+package one.microstream.storage.configuration;
+
+
+import static one.microstream.X.notNull;
+import static one.microstream.chars.XChars.notEmpty;
+
+import java.time.Duration;
+import java.time.format.DateTimeParseException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import one.microstream.bytes.ByteMultiple;
+import one.microstream.storage.exceptions.StorageExceptionInvalidConfiguration;
+
+
+public interface ConfigurationPropertyParser
+{
+	public void parseProperty(String name, String value, Configuration configuration);
+		
+	
+	public static ConfigurationPropertyParser New()
+	{
+		return new Implementation();
+	}
+		
+	
+	public static class Implementation implements ConfigurationPropertyParser, ConfigurationPropertyNames
+	{
+		@Override
+		public void parseProperty(final String name, final String value, final Configuration configuration)
+		{
+			notNull(value);
+
+			switch(notEmpty(name))
+			{
+				case BASE_DIRECTORY:
+				{
+					if(value.startsWith("~/") || value.startsWith("~\\"))
+					{
+						final String directoryInUserHome = value.substring(2);
+						configuration.setBaseDirectoryInUserHome(directoryInUserHome);
+					}
+					else
+					{
+						configuration.setBaseDirectory(notEmpty(value));
+					}
+				}
+				break;
+				
+				case DELETION_DIRECTORY:
+				{
+					configuration.setDeletionDirectory(value);
+				}
+				break;
+				
+				case TRUNCATION_DIRECTORY:
+				{
+					configuration.setTruncationDirectory(value);
+				}
+				break;
+			
+				case CHANNEL_COUNT:
+				{
+					configuration.setChannelCount(Integer.parseInt(value));
+				}
+				break;
+			
+				case CHANNEL_DIRECTORY_PREFIX:
+				{
+					configuration.setChannelDirectoryPrefix(notEmpty(value));
+				}
+				break;
+			
+				case DATA_FILE_PREFIX:
+				{
+					configuration.setDataFilePrefix(notEmpty(value));
+				}
+				break;
+			
+				case DATA_FILE_SUFFIX:
+				{
+					configuration.setDataFileSuffix(notEmpty(value));
+				}
+				break;
+			
+				case TRANSACTION_FILE_PREFIX:
+				{
+					configuration.setTransactionFilePrefix(notEmpty(value));
+				}
+				break;
+			
+				case TRANSACTION_FILE_SUFFIX:
+				{
+					configuration.setTransactionFileSuffix(notEmpty(value));
+				}
+				break;
+			
+				case TYPE_DICTIONARY_FILENAME:
+				{
+					configuration.setTypeDictionaryFilename(notEmpty(value));
+				}
+				break;
+			
+				case TYPE_ID_FILENAME:
+				{
+					configuration.setTypeIdFilename(notEmpty(value));
+				}
+				break;
+			
+				case OBJECT_ID_FILENAME:
+				{
+					configuration.setObjectIdFilename(notEmpty(value));
+				}
+				break;
+			
+				case HOUSEKEEPING_INTERVAL:
+				{
+					configuration.setHouseKeepingInterval(parseDurationMillies(value).toMillis());
+				}
+				break;
+			
+				case HOUSEKEEPING_NANO_TIME_BUDGET:
+				{
+					configuration.setHouseKeepingNanoTimeBudget(parseDurationNanos(value).toNanos());
+				}
+				break;
+			
+				case ENTITY_CACHE_THRESHOLD:
+				{
+					configuration.setEntityCacheThreshold(parseFileSize_long(value));
+				}
+				break;
+			
+				case ENTITY_CACHE_TIMEOUT:
+				{
+					configuration.setEntityCacheTimeout(parseDurationMillies(value).toMillis());
+				}
+				break;
+			
+				case DATA_FILE_MIN_SIZE:
+				{
+					configuration.setDataFileMinSize(parseFileSize_int(value));
+				}
+				break;
+			
+				case DATA_FILE_MAX_SIZE:
+				{
+					configuration.setDataFileMaxSize(parseFileSize_int(value));
+				}
+				break;
+			
+				case DATA_FILE_DISSOLVE_RATIO:
+				{
+					configuration.setDataFileDissolveRatio(Double.parseDouble(value));
+				}
+				break;
+			
+				default:
+					throw new StorageExceptionInvalidConfiguration("Unknown property: " + name);
+			}
+		}
+		
+		
+		protected Duration parseDurationMillies(final String value)
+		{
+			try
+			{
+				return Duration.parse(value);
+			}
+			catch(final DateTimeParseException e)
+			{
+				try
+				{
+					return Duration.ofMillis(Long.parseLong(value));
+				}
+				catch(final NumberFormatException e1)
+				{
+				}
+			}
+			
+			throw new StorageExceptionInvalidConfiguration("Invalid duration: " + value);
+		}
+		
+		
+		protected Duration parseDurationNanos(final String value)
+		{
+			try
+			{
+				return Duration.parse(value);
+			}
+			catch(final DateTimeParseException e)
+			{
+				try
+				{
+					return Duration.ofNanos(Long.parseLong(value));
+				}
+				catch(final NumberFormatException e1)
+				{
+				}
+			}
+			
+			throw new StorageExceptionInvalidConfiguration("Invalid duration: " + value);
+		}
+		
+		
+		protected int parseFileSize_int(final String value)
+		{
+			final long fileSize = parseFileSize_long(value);
+			return fileSize >= Integer.MAX_VALUE ? Integer.MAX_VALUE : (int)fileSize;
+		}
+		
+		
+		protected long parseFileSize_long(final String value)
+		{
+			final Matcher m = Pattern.compile("([\\d.,]+)\\s*(\\w+)",Pattern.CASE_INSENSITIVE)
+					.matcher(value);
+			if(!m.find())
+			{
+				return Long.parseLong(value);
+			}
+			
+			final String amount = m.group(1);
+			final ByteMultiple byteMultiple = ByteMultiple.ofName(m.group(2));
+			if(byteMultiple == null)
+			{
+				throw new StorageExceptionInvalidConfiguration("Invalid file size: " + value);
+			}
+			
+			return byteMultiple.toBytes(Double.parseDouble(amount));
+		}
+	}
+}
