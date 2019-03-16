345c11795662da0f60c7f69c01d9371529f1a4ad
TMuenz <t.muenz@xdev-software.de>
2019-03-16 15:50:52 +0100

    MS-120 devlog and MS-121 JDK bug workaround.

--- work-in-progress/src/one/microstream/test/corp/logic/MainTestArrayLolst.java
+++ work-in-progress/src/one/microstream/test/corp/logic/MainTestArrayLolst.java
@@ -8,12 +8,19 @@ public class MainTestArrayLolst
 {
 	public static void main(final String[] args)
 	{
-		final ArrayList<Integer> ints = new ArrayList<>();
-		ints.ensureCapacity(3);
-		System.out.println(XMemory.accessStorage(ints).length);
+		// bug
+		final ArrayList<Integer> ints1 = new ArrayList<>();
+		ints1.ensureCapacity(10);
+		System.out.println(XMemory.accessStorage(ints1).length);
 		
-		final ArrayList<Integer> ints2 = new ArrayList<>(1);
-		ints2.ensureCapacity(3);
+		// works
+		final ArrayList<Integer> ints2 = new ArrayList<>();
+		ints2.ensureCapacity(11);
 		System.out.println(XMemory.accessStorage(ints2).length);
+		
+		// works
+		final ArrayList<Integer> ints3 = new ArrayList<>(1);
+		ints3.ensureCapacity(10);
+		System.out.println(XMemory.accessStorage(ints3).length);
 	}
 }
