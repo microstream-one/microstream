1fa61ac92a1638efb85ac475d2f36a4832587141
TM <t.muenz@xdev-software.de>
2019-03-21 14:16:21 +0100

    MS-76: TreeMap TypeHandler.

--- persistence.binary/src/one/microstream/java/util/BinaryHandlerTreeMap.java
+++ persistence.binary/src/one/microstream/java/util/BinaryHandlerTreeMap.java
@@ -1,11 +1,11 @@
 package one.microstream.java.util;
 
 import java.util.Comparator;
-import java.util.TreeSet;
+import java.util.TreeMap;
 
 import one.microstream.X;
+import one.microstream.collections.old.KeyValueFlatCollector;
 import one.microstream.collections.old.OldCollections;
-import one.microstream.memory.XMemory;
 import one.microstream.persistence.binary.internal.AbstractBinaryHandlerCustomCollection;
 import one.microstream.persistence.binary.types.Binary;
 import one.microstream.persistence.binary.types.BinaryCollectionHandling;
@@ -16,32 +16,32 @@ import one.microstream.persistence.types.PersistenceObjectIdAcceptor;
 import one.microstream.persistence.types.PersistenceStoreHandler;
 
 
-public final class BinaryHandlerTreeSet extends AbstractBinaryHandlerCustomCollection<TreeSet<?>>
+public final class BinaryHandlerTreeMap extends AbstractBinaryHandlerCustomCollection<TreeMap<?, ?>>
 {
 	///////////////////////////////////////////////////////////////////////////
-	// constants        //
-	/////////////////////
+	// constants //
+	//////////////
 
 	static final long BINARY_OFFSET_COMPARATOR =                           0;
 	static final long BINARY_OFFSET_ELEMENTS   = Binary.objectIdByteLength();
-
-
+	
+	
 
 	///////////////////////////////////////////////////////////////////////////
-	// static methods    //
-	/////////////////////
+	// static methods //
+	///////////////////
 
 	@SuppressWarnings({"unchecked", "rawtypes"})
-	private static Class<TreeSet<?>> typeWorkaround()
+	private static Class<TreeMap<?, ?>> typeWorkaround()
 	{
-		return (Class)TreeSet.class; // no idea how to get ".class" to work otherwise
+		return (Class)TreeMap.class; // no idea how to get ".class" to work otherwise
 	}
-	
-	private static <E> Comparator<? super E> getComparator(final TreeSet<E> instance)
+
+	static final int getElementCount(final Binary bytes)
 	{
-		return XMemory.accessComparator(instance);
+		return X.checkArrayRange(bytes.getListElementCountReferences(BINARY_OFFSET_ELEMENTS));
 	}
-	
+		
 	@SuppressWarnings("unchecked")
 	private static <E> Comparator<? super E> getComparator(
 		final Binary                 bytes  ,
@@ -51,26 +51,20 @@ public final class BinaryHandlerTreeSet extends AbstractBinaryHandlerCustomColle
 		return (Comparator<? super E>)handler.lookupObject(bytes.get_long(BINARY_OFFSET_COMPARATOR));
 	}
 
-	static final int getElementCount(final Binary bytes)
-	{
-		return X.checkArrayRange(bytes.getListElementCountReferences(BINARY_OFFSET_ELEMENTS));
-	}
-
 
 
 	///////////////////////////////////////////////////////////////////////////
-	// constructors     //
-	/////////////////////
+	// constructors //
+	/////////////////
 
-	public BinaryHandlerTreeSet()
+	public BinaryHandlerTreeMap()
 	{
 		super(
 			typeWorkaround(),
-			BinaryCollectionHandling.simpleArrayPseudoFields(
+			BinaryCollectionHandling.keyValuesPseudoFields(
 				pseudoField(Comparator.class, "comparator")
 			)
 		);
-		
 	}
 
 
@@ -82,37 +76,36 @@ public final class BinaryHandlerTreeSet extends AbstractBinaryHandlerCustomColle
 	@Override
 	public final void store(
 		final Binary                  bytes   ,
-		final TreeSet<?>              instance,
+		final TreeMap<?, ?>           instance,
 		final long                    objectId,
 		final PersistenceStoreHandler handler
 	)
 	{
 		// store elements simply as array binary form
-		final long contentAddress = bytes.storeSizedIterableAsList(
+		final long contentAddress = bytes.storeMapEntrySet(
 			this.typeId()         ,
 			objectId              ,
 			BINARY_OFFSET_ELEMENTS,
-			instance              ,
-			instance.size()       ,
+			instance.entrySet()   ,
 			handler
 		);
 		
 		bytes.store_long(
 			contentAddress + BINARY_OFFSET_COMPARATOR,
-			handler.apply(getComparator(instance))
+			handler.apply(instance.comparator())
 		);
 	}
 	
 	@Override
-	public final TreeSet<?> create(final Binary bytes, final PersistenceLoadHandler handler)
+	public final TreeMap<?, ?> create(final Binary bytes, final PersistenceLoadHandler handler)
 	{
-		return new TreeSet<>(
+		return new TreeMap<>(
 			getComparator(bytes, handler)
 		);
 	}
 
 	@Override
-	public final void update(final Binary bytes, final TreeSet<?> instance, final PersistenceLoadHandler handler)
+	public final void update(final Binary bytes, final TreeMap<?, ?> instance, final PersistenceLoadHandler handler)
 	{
 		instance.clear();
 		
@@ -120,22 +113,23 @@ public final class BinaryHandlerTreeSet extends AbstractBinaryHandlerCustomColle
 		 * Tree collections don't use hashing, but their comparing logic still uses the elements' state,
 		 * which might not yet be available when this method is called. Hence the detour to #complete.
 		 */
-		final Object[] elementsHelper = new Object[getElementCount(bytes)];
-		bytes.collectElementsIntoArray(BINARY_OFFSET_ELEMENTS, handler, elementsHelper);
-		bytes.registerHelper(instance, elementsHelper);
-	}
-	
-	@Override
-	public final void complete(final Binary bytes, final TreeSet<?> instance, final PersistenceLoadHandler handler)
-	{
-		OldCollections.populateSetFromHelperArray(instance, bytes.getHelper(instance));
+		final int elementCount = getElementCount(bytes);
+		final KeyValueFlatCollector<Object, Object> collector = KeyValueFlatCollector.New(elementCount);
+		bytes.collectKeyValueReferences(BINARY_OFFSET_ELEMENTS, elementCount, handler, collector);
+		bytes.registerHelper(instance, collector.yield());
 	}
 
 	@Override
-	public final void iterateInstanceReferences(final TreeSet<?> instance, final PersistenceFunction iterator)
+	public final void iterateInstanceReferences(final TreeMap<?, ?> instance, final PersistenceFunction iterator)
 	{
-		iterator.apply(getComparator(instance));
-		Persistence.iterateReferencesIterable(iterator, instance);
+		iterator.apply(instance.comparator());
+		Persistence.iterateReferencesMap(iterator, instance);
+	}
+
+	@Override
+	public final void complete(final Binary bytes, final TreeMap<?, ?> instance, final PersistenceLoadHandler builder)
+	{
+		OldCollections.populateMapFromHelperArray(instance, bytes.getHelper(instance));
 	}
 
 	@Override
