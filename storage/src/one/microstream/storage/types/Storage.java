1af845db0808e6bf3bc14751b1378f615fa810c6
TM <t.muenz@xdev-software.de>
2019-05-08 16:58:14 +0200

    JavaDoc.

--- storage/src/one/microstream/storage/types/Storage.java
+++ storage/src/one/microstream/storage/types/Storage.java
@@ -11,7 +11,6 @@ import java.io.File;
  * To setup and start a database, see the class "EmbeddedStorage".
  * 
  * @author TM
- *
  */
 public final class Storage
 {
@@ -36,7 +35,8 @@ public final class Storage
 	///////////////////
 	
 	/**
-	 * Returns the dummy file number for transaction files, which is the value {@value #TRANSACTIONS_FILE_NUMBER}.<p>
+	 * Returns the dummy file number for transaction files, which is the value {@value Storage#TRANSACTIONS_FILE_NUMBER}.
+	 * <p>
 	 * Transaction files conceptually don't have a file number, but are subject to the {@link StorageNumberedFile}
 	 * mechanics, so a dummy value is required. Since transaction files are planned to be replaced in the future
 	 * by meta data inlined directly in the storage files, a dummy value like this is a preferable solution
@@ -51,14 +51,14 @@ public final class Storage
 	
 	/**
 	 * Checks if the passed {@link StorageNumberedFile} is a transaction file by comparing its file number to
-	 * {@link #transactionsFileNumber()}.
+	 * {@link Storage#transactionsFileNumber()}.
 	 * 
 	 * @param file the {@link StorageNumberedFile} to be checked.
 	 * 
 	 * @return whether the passed file is a transactions file.
 	 * 
-	 * @see #transactionsFileNumber()
-	 * @see #isDataFile(StorageNumberedFile)
+	 * @see Storage#transactionsFileNumber()
+	 * @see Storage#isDataFile(StorageNumberedFile)
 	 */
 	public static final boolean isTransactionFile(final StorageNumberedFile file)
 	{
@@ -72,7 +72,7 @@ public final class Storage
 	 * 
 	 * @return whether the passed file is a data file.
 	 * 
-	 * @see #isTransactionFile(StorageNumberedFile)
+	 * @see Storage#isTransactionFile(StorageNumberedFile)
 	 */
 	public static final boolean isDataFile(final StorageNumberedFile file)
 	{
@@ -82,12 +82,12 @@ public final class Storage
 	/**
 	 * Pseudo-constructor method to create a new {@link StorageFileProvider} instance with a default value
 	 * for the storage directory, which is the folder named "storage" in the JVM's working directory.<p>
-	 * To specify a custom storage directory, see #FileProvider(File).<p>
+	 * To specify a custom storage directory, see Storage#FileProvider(File).<p>
 	 * For full control over defining a storage's file locations and names, see {@link StorageFileProvider.Builder}.
 	 * 
 	 * @return a new {@link StorageFileProvider} instance with the default storage directory.
 	 * 
-	 * @see #FileProvider(File)
+	 * @see Storage#FileProvider(File)
 	 * @see StorageFileProvider.Builder
 	 */
 	public static final StorageFileProvider FileProvider()
@@ -104,7 +104,7 @@ public final class Storage
 	 * 
 	 * @return a new {@link StorageFileProvider} instance with the passed {@link File} as the storage directory.
 	 * 
-	 * @see #FileProvider(File)
+	 * @see Storage#FileProvider(File)
 	 * @see StorageFileProvider.Builder
 	 */
 	public static final StorageFileProvider FileProvider(final File storageDirectory)
@@ -175,18 +175,47 @@ public final class Storage
 	/**
 	 * Pseudo-constructor method to create a new {@link StorageHousekeepingController} instance
 	 * using default values.<p>
-	 * To specify custom values, see {@link #HousekeepingController(long, long)}.<p>
+	 * To specify custom values, see {@link Storage#HousekeepingController(long, long)}.<p>
 	 * 
 	 * @return a new {@link StorageHousekeepingController} instance using default values.
 	 * 
-	 * @see #HousekeepingController(long, long)
-	 * @see StorageHousekeepingController#New())
+	 * @see Storage#HousekeepingController(long, long)
+	 * @see StorageHousekeepingController#New()
 	 */
 	public static final StorageHousekeepingController HousekeepingController()
 	{
 		return StorageHousekeepingController.New();
 	}
 	
+
+	/**
+	 * Pseudo-constructor method to create a new {@link StorageHousekeepingController} instance
+	 * using the passed values.<p>
+	 * The combination of these two values can be used to define how much percentage of the system's computing power
+	 * shall be used for storage housekeeping. Example: 10 Million ns (= 10 ms) housekeeping budget every 1000 ms
+	 * means (roughly) 1% of the computing power will be used for storage housekeeping.<p>
+	 * Note that in an application where no store occures over a longer period of time will eventually complete
+	 * all housekeeping tasks, which reduces the required computing power to 0. When the next store occurs, the
+	 * housekeeping starts anew.<br>
+	 * How long the housekeeping requires to complete depends on the computing power it is granted here, other
+	 * configurations (like cache timeouts) and the amount of data that has to be managed.
+	 * <p>
+	 * See all "issue~" methods in {@link StorageConnection} for a way to call housekeeping explicitly and causing
+	 * it to be executed completely.
+	 * 
+	 * 
+	 * @param housekeepingIntervalMs the interval in milliseconds that the storage threads shall
+	 *        execute their various housekeeping actions (like cache clearing checks, file consolidation, etc.)
+	 * 
+	 * @param housekeepingTimeBudgetNs the time budget in nanoseconds that each storage thread will use to perform
+	 *        a housekeeping action. This is a best effort value, not a strictly reliable border value. This means
+	 *        a housekeeping action can occasionally take slightly longer than specified here.
+	 * 
+	 * @return a new {@link StorageHousekeepingController} instance using default values.
+	 * 
+	 * @see Storage#HousekeepingController()
+	 * @see StorageHousekeepingController#New(long, long)
+	 */
 	public static final StorageHousekeepingController HousekeepingController(
 		final long housekeepingIntervalMs  ,
 		final long housekeepingTimeBudgetNs
@@ -195,21 +224,36 @@ public final class Storage
 		return StorageHousekeepingController.New(housekeepingIntervalMs, housekeepingTimeBudgetNs);
 	}
 
+	/**
+	 * Pseudo-constructor method to create a new {@link StorageEntityCacheEvaluator} instance
+	 * using default values.<p>
+	 * To specify custom values, see {@link Storage#EntityCacheEvaluator(long, long)}.<p>
+	 * 
+	 * @return a new {@link StorageEntityCacheEvaluator} instance using default values.
+	 * 
+	 * @see Storage#EntityCacheEvaluator(long)
+	 * @see Storage#EntityCacheEvaluator(long, long)
+	 * @see StorageEntityCacheEvaluator#New()
+	 */
 	public static final StorageEntityCacheEvaluator EntityCacheEvaluator()
 	{
 		return StorageEntityCacheEvaluator.New();
 	}
 
-	public static final StorageChannelCountProvider ChannelCountProvider()
-	{
-		return StorageChannelCountProvider.New();
-	}
-
-	public static final StorageChannelCountProvider ChannelCountProvider(final int channelCount)
-	{
-		return StorageChannelCountProvider.New(channelCount);
-	}
-
+	/**
+	 * Pseudo-constructor method to create a new {@link StorageEntityCacheEvaluator} instance
+	 * using the passed {@code timeoutMs} value and a default threshold value.<br>
+	 * See {@link Storage#EntityCacheEvaluator(long, long)}.<p>
+	 * 
+	 * @param timeoutMs the time (in milliseconds) of not being read (the "age"), after which a particular
+	 *        entity's data will be cleared from the Storage's internal cache.
+	 * 
+	 * @return a new {@link StorageEntityCacheEvaluator} instance.
+	 * 
+	 * @see Storage#EntityCacheEvaluator()
+	 * @see Storage#EntityCacheEvaluator(long, long)
+	 * @see StorageEntityCacheEvaluator#New(long)
+	 */
 	public static final StorageEntityCacheEvaluator EntityCacheEvaluator(
 		final long timeoutMs
 	)
@@ -217,12 +261,57 @@ public final class Storage
 		return StorageEntityCacheEvaluator.New(timeoutMs);
 	}
 
+	/**
+	 * Pseudo-constructor method to create a new {@link StorageEntityCacheEvaluator} instance
+	 * using the passed values.
+	 * 
+	 * @param timeoutMs the time (in milliseconds) of not being read (the "age"), after which a particular
+	 *        entity's data will be cleared from the Storage's internal cache.
+	 * 
+	 * @param threshold an abstract value to evaluate the product of size and age of an entity in relation to the
+	 *        current cache size in order to determine if the entity's data shall be cleared from the cache.
+	 * 
+	 * @return a new {@link StorageEntityCacheEvaluator} instance.
+	 * 
+	 * @see Storage#EntityCacheEvaluator()
+	 * @see Storage#EntityCacheEvaluator(long)
+	 * @see StorageEntityCacheEvaluator#New(long, long)
+	 */
 	public static final StorageEntityCacheEvaluator EntityCacheEvaluator(
 		final long timeoutMs,
 		final long threshold
 	)
 	{
 		return StorageEntityCacheEvaluator.New(timeoutMs, threshold);
+	}
+
+
+	/**
+	 * Pseudo-constructor method to create a new {@link StorageChannelCountProvider} instance
+	 * using the default value of 1 (meaning a single storage thread).<p>
+	 * To specify a custom value, see {@link Storage#ChannelCountProvider(int)}.<p>
+	 * 
+	 * @return a new {@link StorageChannelCountProvider} instance defining a single-threaded storage.
+	 * 
+	 * @see Storage#ChannelCountProvider(int)
+	 * @see StorageChannelCountProvider#New()
+	 */
+	public static final StorageChannelCountProvider ChannelCountProvider()
+	{
+		return StorageChannelCountProvider.New();
+	}
+
+	/**
+	 * Pseudo-constructor method to create a new {@link StorageChannelCountProvider} instance
+	 * using the passed value to specify the amount of channels (threads and their exclusive storage sub-directory).<p>
+	 * 
+	 * @param channelCount the amount of channels (threads and their exclusive storage sub-directory).
+	 * 
+	 * @return a new {@link StorageChannelCountProvider} instance using the passed {@code channelCount}.
+	 */
+	public static final StorageChannelCountProvider ChannelCountProvider(final int channelCount)
+	{
+		return StorageChannelCountProvider.New(channelCount);
 	}
 	
 	public static final StorageDataFileEvaluator DataFileEvaluator()
@@ -292,7 +381,7 @@ public final class Storage
 	
 
 	/**
-	 * Calls {@link #consolidate(StorageConnection, StorageDataFileDissolvingEvaluator, StorageEntityCacheEvaluator)}
+	 * Calls {@link Storage#consolidate(StorageConnection, StorageDataFileDissolvingEvaluator, StorageEntityCacheEvaluator)}
 	 * with {@literal null} as additional parameters (causing live configuration to be used instead).
 	 *
 	 * @param storageConnection the connection to the storage that shall be consolidated.
