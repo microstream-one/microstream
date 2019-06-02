eeeb8f5a3bd7679514bc694f3cfdc6939d9f9a34
TMuenz <t.muenz@xdev-software.de>
2019-06-02 02:43:58 +0200

    Util Helper

--- work-in-progress/src/one/microstream/storage/util/FileMoveSyncher.java
+++ work-in-progress/src/one/microstream/storage/util/FileMoveSyncher.java
@@ -1,10 +1,18 @@
 package one.microstream.storage.util;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.function.Function;
 
 import one.microstream.collections.EqHashTable;
 import one.microstream.collections.HashEnum;
+import one.microstream.collections.types.XGettingCollection;
+import one.microstream.files.XFiles;
+import one.microstream.meta.XDebug;
+import one.microstream.typing.KeyValue;
+
 
 public class FileMoveSyncher
 {
@@ -13,6 +21,7 @@ public class FileMoveSyncher
 	//////////////
 	
 	private static final Function<String, HashEnum<File>> FILE_INDEX_SUPPLIER = f -> HashEnum.New();
+	
 	
 	
 	///////////////////////////////////////////////////////////////////////////
@@ -27,6 +36,14 @@ public class FileMoveSyncher
 		}
 		
 		throw new RuntimeException("Not a directory: " + file);
+	}
+	
+	public static Function<File, String> fileIdentitySimpleNameSizeChangeTime()
+	{
+		return (final File file) ->
+		{
+			return file.getName() + " $" + file.length() + " @" + file.lastModified();
+		};
 	}
 	
 	
@@ -61,48 +78,105 @@ public class FileMoveSyncher
 		mustDirectory(targetDirectory);
 		
 		final EqHashTable<String, HashEnum<File>> indexedFiles = EqHashTable.New();
-		indexFiles(targetDirectory, indexedFiles, this.fileIdentifier);
 		
+		XDebug.println("Indexing files ...");
+		indexFiles(targetDirectory, indexedFiles, this.fileIdentifier);
+		XDebug.println("Indexed unique files: " + indexedFiles.size());
+		
+//		checkForDuplicates(indexedFiles);
+		
+		XDebug.println("Synching files ...");
 		final String sourceDirectoryBase = sourceDirectory.getAbsolutePath();
-		synchMoveFiles(sourceDirectoryBase, sourceDirectory, targetDirectory, indexedFiles, this.fileIdentifier);
+		synchMoveDirectoryContent(
+			sourceDirectoryBase,
+			sourceDirectory,
+			targetDirectory,
+			sourceDirectoryBase.length(),
+			targetDirectory.getAbsolutePath().length(),
+			indexedFiles,
+			this.fileIdentifier
+		);
 	}
 	
 	static final void indexFiles(
 		final File                                directory     ,
-		final EqHashTable<String, HashEnum<File>> indexFiles    ,
+		final EqHashTable<String, HashEnum<File>> indexedFiles  ,
 		final Function<File, String>              fileIdentifier
 	)
 	{
+//		XDebug.println("Indexing directory " + directory);
+		
 		final File[] files = directory.listFiles();
 		for(final File file : files)
 		{
-			if(!file.isDirectory())
+			if(file.isDirectory())
 			{
 				continue;
 			}
 			
 			final String fileIdentity = fileIdentifier.apply(file);
-			indexFiles.ensure(fileIdentity, FILE_INDEX_SUPPLIER).add(file);
+			indexedFiles.ensure(fileIdentity, FILE_INDEX_SUPPLIER).add(file);
+			if(indexedFiles.size() % 1000 == 0)
+			{
+				XDebug.println(indexedFiles.size() + " files processed.");
+			}
 		}
 		for(final File file : files)
 		{
 			if(file.isDirectory())
 			{
-				indexFiles(file, indexFiles, fileIdentifier);
+				indexFiles(file, indexedFiles, fileIdentifier);
 			}
 		}
 	}
 	
-	static final void synchMoveFiles(
-		final String                              sourceDirectoryBase,
-		final File                                sourceDirectory    ,
-		final File                                targetDirectoryBase,
+	static final void checkForDuplicates(
+		final EqHashTable<String, HashEnum<File>> indexedFiles
+	)
+	{
+		for(final KeyValue<String, HashEnum<File>> e : indexedFiles)
+		{
+			if(e.value().size() > 1)
+			{
+				System.out.println("Duplicates:");
+				e.value().iterate(System.out::println);
+				System.out.println();
+			}
+		}
+	}
+	
+	private static String getRelativePath(final File f, final int directoryBaseLength)
+	{
+		return f.getAbsolutePath().substring(directoryBaseLength);
+	}
+	
+	static final void synchMoveDirectoryContent(
+		final String                              srcDirBase     ,
+		final File                                sourceDirectory,
+		final File                                trgDirBase     ,
+		final int                                 srcBaseLength  ,
+		final int                                 trgBaseLength  ,
 		final EqHashTable<String, HashEnum<File>> indexFiles     ,
 		final Function<File, String>              fileIdentifier
 	)
 	{
 		final File[] sourceFiles = sourceDirectory.listFiles();
-		
+		final String relativeSourcePath = getRelativePath(sourceDirectory, srcBaseLength);
+		synchMoveRealFiles(srcDirBase, trgDirBase, srcBaseLength, trgBaseLength, relativeSourcePath, sourceFiles, indexFiles, fileIdentifier);
+		synchMoveSubDirs(srcDirBase, trgDirBase, srcBaseLength, trgBaseLength, sourceFiles, indexFiles, fileIdentifier);
+	}
+	
+	static final void synchMoveRealFiles(
+		final String                              sourceDirectoryBase,
+		final File                                targetDirectoryBase,
+		final int                                 srcBaseLength      ,
+		final int                                 trgBaseLength      ,
+		final String                              relativeSourcePath ,
+		final File[]                              sourceFiles        ,
+		final EqHashTable<String, HashEnum<File>> indexFiles         ,
+		final Function<File, String>              fileIdentifier
+	)
+	{
 		for(final File sourceFile : sourceFiles)
 		{
 			if(sourceFile.isDirectory())
@@ -110,13 +184,68 @@ public class FileMoveSyncher
 				continue;
 			}
 			
-			final String sourceFileIdentity = fileIdentifier.apply(sourceFile);
-			final HashEnum<File> matchingTargetFiles = indexFiles.get(sourceFileIdentity);
-			//  - select most suited file (already fitting path, otherwise naively first file)
-			//  - move selected file to fitting path (or no-op)
-			//  - handle rest (warn / delete, maybe functional)
+			final String  sourceFileIdentity = fileIdentifier.apply(sourceFile);
+			final HashEnum<File> targetFiles = indexFiles.get(sourceFileIdentity);
+			final File    matchingTargetFile = searchMatchingFile(sourceFile, targetFiles, srcBaseLength, trgBaseLength);
+			if(matchingTargetFile == null)
+			{
+				// copy source file? But then this tool becomes a complete file syncher instead of just a move-syncher.
+				continue;
+			}
+			
+			final File newTargetDirectory = new File(targetDirectoryBase, relativeSourcePath);
+			final File newTargetFile      = new File(newTargetDirectory, matchingTargetFile.getName());
+			
+			// (01.06.2019 TM)FIXME: /!\ DEBUG
+			System.out.println("$ " + sourceFile.getAbsolutePath());
+			System.out.println("> " + newTargetFile.getAbsolutePath());
+			System.out.println("< " + matchingTargetFile.getAbsolutePath());
+			System.out.println();
+			
+			move(matchingTargetFile, newTargetFile);
+			
+			// handle other files in enum? (warn / delete, maybe functional)
 		}
-		
+	}
+	
+	static void move(
+		final File targetSourceFile,
+		final File targetTargetFile
+	)
+	{
+		XFiles.ensureDirectory(targetTargetFile.getParentFile());
+		if(targetTargetFile.exists())
+		{
+			System.out.println("x already exists: " + targetTargetFile);
+			System.out.println();
+			return;
+		}
+
+		System.out.println("Moving " + targetSourceFile);
+		System.out.println(" to -> " + targetTargetFile);
+
+		final Path source = targetSourceFile.toPath();
+		final Path target = targetTargetFile.toPath();
+		try
+		{
+			Files.move(source, target);
+		}
+		catch(final IOException e)
+		{
+			throw new RuntimeException(e);
+		}
+	}
+	
+	static final void synchMoveSubDirs(
+		final String                              sourceDirectoryBase,
+		final File                                targetDirectoryBase,
+		final int                                 srcBaseLength      ,
+		final int                                 trgBaseLength      ,
+		final File[]                              sourceFiles        ,
+		final EqHashTable<String, HashEnum<File>> indexFiles         ,
+		final Function<File, String>              fileIdentifier
+	)
+	{
 		for(final File sourceFile : sourceFiles)
 		{
 			if(!sourceFile.isDirectory())
@@ -124,14 +253,56 @@ public class FileMoveSyncher
 				continue;
 			}
 			
-			final String sourceFilePath = sourceFile.getAbsolutePath();
-			final File targetDirectory = new File(
+			synchMoveDirectoryContent(
+				sourceDirectoryBase,
+				sourceFile,
 				targetDirectoryBase,
-				sourceFilePath.substring(sourceDirectoryBase.length())
+				srcBaseLength,
+				trgBaseLength,
+				indexFiles,
+				fileIdentifier
 			);
-			
-			synchMoveFiles(sourceDirectoryBase, sourceFile, targetDirectory, indexFiles, fileIdentifier);
 		}
+	}
+	
+	static File searchMatchingFile(
+		final File                               sourceFile               ,
+		final XGettingCollection<? extends File> targetFiles              ,
+		final int                                sourceDirectoryBaseLength,
+		final int                                targetDirectoryBaseLength
+	)
+	{
+		if(targetFiles == null)
+		{
+			return null;
+		}
+
+		final String relativeSourcePath = getRelativePath(sourceFile.getParentFile(), sourceDirectoryBaseLength);
+
+		// select most suited file ? (e.g. by relative path backwards)
+		for(final File f : targetFiles)
+		{
+			final String relativeTargetPath = getRelativePath(f.getParentFile(), targetDirectoryBaseLength);
+			if(relativeSourcePath.equals(relativeTargetPath))
+			{
+				// if there is already a matching file, discard all other potential matches
+				return null;
+			}
+		}
+		
+		// naive strategy: the first file is used.
+		return targetFiles.get();
+	}
+	
+	
+	
+	public static void main(final String[] args)
+	{
+		final FileMoveSyncher fms = new FileMoveSyncher(FileMoveSyncher.fileIdentitySimpleNameSizeChangeTime());
+		fms.moveSynch(
+			new File("G:\\media"),
+			new File("H:\\media")
+		);
 	}
 	
 }
