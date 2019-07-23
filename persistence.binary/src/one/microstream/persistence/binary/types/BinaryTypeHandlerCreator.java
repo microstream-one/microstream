62451d26fc1088efde5039d8deb0b1cf539746df
TM <t.muenz@xdev-software.de>
2019-07-23 14:45:36 +0200

    Disabled added exception for Enum types for existing projects until the proper handling of Enum types is completed.

--- persistence.binary/src/one/microstream/persistence/binary/types/BinaryTypeHandlerCreator.java
+++ persistence.binary/src/one/microstream/persistence/binary/types/BinaryTypeHandlerCreator.java
@@ -99,12 +99,14 @@ public interface BinaryTypeHandlerCreator extends PersistenceTypeHandlerCreator<
 				 */
 //				return this.createEnumHandler(type, persistableFields);
 				// (12.07.2019 TM)EXCP: proper exception
-				throw new RuntimeException(
-					"Handling Java language enums is currently not supported since changes to the enum constants,"
-					+ " a part of the type definition, would require changes to data and might even be ambiguous."
-					+ " Please consider that enums are merely a syntax sugar helper for building logic,"
-					+ " not a suitable construct to be used in a persisted entity graph."
-				);
+				
+				// (23.07.2019 TM)NOTE: temporarily disabled for existing projects until proper handling is implemented.
+//				throw new RuntimeException(
+//					"Handling Java language enums is currently not supported since changes to the enum constants,"
+//					+ " a part of the type definition, would require changes to data and might even be ambiguous."
+//					+ " Please consider that enums are merely a syntax sugar helper for building logic,"
+//					+ " not a suitable construct to be used in a persisted entity graph."
+//				);
 			}
 
 			// default implementation simply always uses a blank memory instantiator
