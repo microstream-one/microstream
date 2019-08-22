f8684fc1c66199f05fc21819e318e74cf1bb96ec
TM <t.muenz@xdev-software.de>
2019-08-22 15:11:04 +0200

    "Deactivated" the class DEBUGStorage.

--- work-in-progress/src/one/microstream/persistence/test/MainTestMergeFiles.java
+++ work-in-progress/src/one/microstream/persistence/test/MainTestMergeFiles.java
@@ -2,13 +2,15 @@ package one.microstream.persistence.test;
 
 import java.io.File;
 
-import one.microstream.storage.types.DEBUGStorage;
+import one.microstream.X;
+import one.microstream.collections.types.XList;
+import one.microstream.files.XFiles;
 
 public class MainTestMergeFiles
 {
 	public static void main(final String[] args)
 	{
-		DEBUGStorage.mergeStorageFiles(
+		mergeStorageFiles(
 			new File("d:/merged.dat"),
 			".dat",
 			new File("D:/Bonus25/storage/channel_0"),
@@ -16,5 +18,22 @@ public class MainTestMergeFiles
 			new File("D:/Bonus25/storage/channel_2"),
 			new File("D:/Bonus25/storage/channel_3")
 		);
+	}
+
+	public static final void mergeStorageFiles(
+		final File    targetFile        ,
+		final String  fileSuffix        ,
+		final File... channelDirectories
+	)
+	{
+		XFiles.ensureWriteableFile(targetFile);
+
+		final XList<File> sourceFiles = X.List();
+		for(final File file : channelDirectories)
+		{
+			sourceFiles.addAll(file.listFiles());
+		}
+
+		XFiles.mergeBinary(sourceFiles, targetFile, f -> f.getPath().endsWith(fileSuffix));
 	}
 }
