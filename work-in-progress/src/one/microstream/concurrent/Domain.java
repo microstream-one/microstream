a5deb5c10fbe52dbba7f775eebcde650efb5401e
TM <t.muenz@xdev-software.de>
2019-09-27 17:22:17 +0200

    Concurrency framework conception and implementation.

--- work-in-progress/src/one/microstream/concurrent/Domain.java
+++ work-in-progress/src/one/microstream/concurrent/Domain.java
@@ -3,60 +3,6 @@ package one.microstream.concurrent;
 public interface Domain<E>
 {
 	public <R> R executeLogic(final DomainLogic<? super E, R> logic);
-	
-	
-	
-	public final class Default<E> implements EnqueingDomain<E>
-	{
-		///////////////////////////////////////////////////////////////////////////
-		// instance fields //
-		////////////////////
 		
-		private final Thread owner     ;
-		private final E      rootEntity;
-		
-		
-		
-		///////////////////////////////////////////////////////////////////////////
-		// constructors //
-		/////////////////
-		
-		Default(final Thread owner, final E rootEntity)
-		{
-			super();
-			this.owner = owner;
-			this.rootEntity = rootEntity;
-		}
-		
-		
-		
-		///////////////////////////////////////////////////////////////////////////
-		// methods //
-		////////////
-		
-		@Override
-		public <R> R executeLogic(final DomainLogic<? super E, R> logic)
-		{
-			// core part of the whole concept: only the owner thread may execute a logic on a domain.
-			if(Thread.currentThread() != this.owner)
-			{
-				throw new RuntimeException(); // (26.09.2019 TM)EXCP: proper exception
-			}
-			
-			return logic.executeDomainLogic(this.rootEntity);
-		}
-
-
-
-		@Override
-		public void enqueueTask(final DomainTask<? super E, ?> task)
-		{
-			task.executeOn(this.rootEntity);
-			
-			throw new one.microstream.meta.NotImplementedYetError(); // FIXME EnqueingDomain<E>#enqueueTask()
-		}
-		
-	}
-	
 }
 
