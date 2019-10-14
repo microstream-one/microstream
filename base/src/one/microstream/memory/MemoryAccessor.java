e41c7b7662ed9230e42e5eb6e52f83622f6c5eba
TM <t.muenz@xdev-software.de>
2019-10-14 17:23:26 +0200

    priv#111: XMemory splitting (work in progress)

--- base/src/one/microstream/memory/MemoryAccessor.java
+++ base/src/one/microstream/memory/MemoryAccessor.java
@@ -1,28 +1,49 @@
 package one.microstream.memory;
 
+import java.lang.reflect.Field;
+
 public interface MemoryAccessor
 {
 	public void set_long(long address, long value);
 	
 	
-	// no one knows why this method is called Sun ... shhhhh...
-	public static MemoryAccessor Sun()
-	{
-		return new MemoryAccessor.Sun();
-	}
-	
-	public final class Sun implements MemoryAccessor
-	{
-		Sun()
-		{
-			super();
-		}
+	public long byteSizeArray_byte(long elementCount);
 
-		@Override
-		public final void set_long(final long address, final long value)
-		{
-			XMemory.VM.putLong(address, value);
-		}
+	public long byteSizeArray_boolean(long elementCount);
+
+	public long byteSizeArray_short(long elementCount);
+
+	public long byteSizeArray_char(long elementCount);
+
+	public long byteSizeArray_int(long elementCount);
+
+	public long byteSizeArray_float(long elementCount);
+
+	public long byteSizeArray_long(long elementCount);
+
+	public long byteSizeArray_double(long elementCount);
+
+	public long byteSizeArrayObject(long elementCount);
+	
+	
+	
+	// (14.10.2019 TM)FIXME: priv#111: not sure if needed
+//	public Object getStaticReference(Field field);
+	
+//	public Object getStaticFieldBase(Field field);
+
+	// (14.10.2019 TM)FIXME: objectFieldOffset can't stay here. It has to be wrapped in a plattform-specific type using it.
+	public long objectFieldOffset(Field field);
+	
+	
+	
+	// (14.10.2019 TM)FIXME: priv#111: all methods below are only used for debugging, not productive code
+	
+	public int byteSizeReference();
+	
+	public int byteSizeInstance(Class<?> type);
+	
+	public int byteSizeFieldValue(Class<?> type);
+	
 		
-	}
 }
