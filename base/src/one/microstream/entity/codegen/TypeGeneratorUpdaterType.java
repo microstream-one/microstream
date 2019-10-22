963fd941e888bee9dac1e3e24e9e23c26b2d2116
FH <f.habermann@microstream.one>
2019-10-22 17:55:56 +0200

    Layered entities: added generator for appendable (toString, debuggability)

--- base/src/one/microstream/entity/codegen/TypeGeneratorUpdaterType.java
+++ base/src/one/microstream/entity/codegen/TypeGeneratorUpdaterType.java
@@ -4,7 +4,6 @@ package one.microstream.entity.codegen;
 import java.beans.Introspector;
 import java.util.List;
 
-import javax.annotation.processing.ProcessingEnvironment;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.element.TypeParameterElement;
 
@@ -19,12 +18,12 @@ class TypeGeneratorUpdaterType extends TypeGenerator
 	final static String SUFFIX = "Updater";
 	
 	TypeGeneratorUpdaterType(
-		final ProcessingEnvironment processingEnv,
+		final EntityProcessor processor,
 		final TypeElement entityTypeElement,
 		final List<Member> members)
 	{
 		super(
-			processingEnv,
+			processor,
 			entityTypeElement,
 			members,
 			true,
