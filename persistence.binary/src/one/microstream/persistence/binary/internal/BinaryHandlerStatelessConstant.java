60531f35c80c6595d565d94904541535081e2d7d
TM <t.muenz@xdev-software.de>
2019-11-21 13:37:49 +0100

    Implemented BinaryHandlerStatelessConstant to further account for JDK constant instance super duper special cases.

---
+++ persistence.binary/src/one/microstream/persistence/binary/internal/BinaryHandlerStatelessConstant.java
@@ -0,0 +1,67 @@
+package one.microstream.persistence.binary.internal;
+
+import static one.microstream.X.notNull;
+
+import one.microstream.persistence.binary.types.Binary;
+import one.microstream.persistence.types.PersistenceObjectIdResolver;
+import one.microstream.persistence.types.PersistenceStoreHandler;
+import one.microstream.reflect.XReflect;
+
+
+public final class BinaryHandlerStatelessConstant<T> extends AbstractBinaryHandlerTrivial<T>
+{
+	///////////////////////////////////////////////////////////////////////////
+	// static methods //
+	///////////////////
+	
+	public static <T> BinaryHandlerStatelessConstant<T> New(final T constantInstance)
+	{
+		return new BinaryHandlerStatelessConstant<>(
+			notNull(constantInstance)
+		);
+	}
+	
+	
+	
+	///////////////////////////////////////////////////////////////////////////
+	// instance fields //
+	////////////////////
+	
+	private final T constantInstance;
+	
+	
+	
+	///////////////////////////////////////////////////////////////////////////
+	// constructors //
+	/////////////////
+
+	BinaryHandlerStatelessConstant(final T constantInstance)
+	{
+		super(XReflect.getClass(constantInstance));
+		this.constantInstance = constantInstance;
+	}
+	
+	
+	
+	///////////////////////////////////////////////////////////////////////////
+	// methods //
+	////////////
+
+	@Override
+	public final void store(
+		final Binary                  bytes   ,
+		final T                       instance,
+		final long                    objectId,
+		final PersistenceStoreHandler handler
+	)
+	{
+		bytes.storeStateless(this.typeId(), objectId);
+	}
+
+	@Override
+	public final T create(final Binary bytes, final PersistenceObjectIdResolver idResolver)
+	{
+		return this.constantInstance;
+	}
+
+}
