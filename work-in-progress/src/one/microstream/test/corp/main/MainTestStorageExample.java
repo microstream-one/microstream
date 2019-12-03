0c16cceede5d06bfbc17f8af11b8fcd009f03695
TM <t.muenz@xdev-software.de>
2019-12-03 09:48:55 +0100

    Cleaned up some MainTest classes.

--- work-in-progress/src/one/microstream/test/corp/main/MainTestStorageExample.java
+++ work-in-progress/src/one/microstream/test/corp/main/MainTestStorageExample.java
@@ -2,11 +2,8 @@ package one.microstream.test.corp.main;
 
 import one.microstream.io.XIO;
 import one.microstream.meta.XDebug;
-import one.microstream.persistence.internal.PersistenceTypeDictionaryFileHandlerArchiving;
 import one.microstream.storage.types.EmbeddedStorage;
 import one.microstream.storage.types.EmbeddedStorageManager;
-import one.microstream.storage.types.Storage;
-import one.microstream.storage.types.StorageBackupSetup;
 import one.microstream.storage.types.StorageFileProvider;
 import one.microstream.test.corp.logic.Test;
 import one.microstream.test.corp.logic.TestImportExport;
@@ -14,37 +11,14 @@ import one.microstream.test.corp.logic.TestImportExport;
 
 public class MainTestStorageExample
 {
-	static final String DIRECTORY_STORAGE   = StorageFileProvider.Defaults.defaultStorageDirectory();
-	static final String DIRECTORY_BACKUP    = DIRECTORY_STORAGE + "/backup";
-	static final String DIRECTORY_DELETED   = DIRECTORY_BACKUP  + "/deleted";
-	static final String DIRECTORY_TRUNCATED = DIRECTORY_BACKUP  + "/truncated";
-	
 	static
 	{
-		XDebug.deleteAllFiles(XIO.Path(StorageFileProvider.Defaults.defaultStorageDirectory()), true);
+//		XDebug.deleteAllFiles(XIO.Path(StorageFileProvider.Defaults.defaultStorageDirectory()), true);
 //		XMemory.setMemoryAccessor(MemoryAccessorGeneric.New(JdkInternals.InstantiatorBlank()));
 	}
 	
 	// creates and starts an embedded storage manager with all-default-settings.
-	static final EmbeddedStorageManager STORAGE = EmbeddedStorage
-		.Foundation(
-			Storage.ConfigurationBuilder()
-			.setDataFileEvaluator(Storage.DataFileEvaluator(1000, 200_000))
-			.setBackupSetup(
-				// the only necessary part to activate and configure backupping.
-				StorageBackupSetup.New(
-					Storage
-					.FileProviderBuilder()
-					.setBaseDirectory(DIRECTORY_BACKUP)
-					.setDeletionDirectory(DIRECTORY_DELETED)
-					.setTruncationDirectory(DIRECTORY_TRUNCATED)
-					.setFileHandlerCreator(PersistenceTypeDictionaryFileHandlerArchiving::New)
-					.createFileProvider()
-				)
-			)
-		)
-		.start()
-	;
+	static final EmbeddedStorageManager STORAGE = EmbeddedStorage.start();
 
 	public static void main(final String[] args)
 	{
