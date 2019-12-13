80225967c257e0107c9a9bd94d5d84d4560cca6d
microstream_hg <h.grunwald@microstream.one>
2019-12-13 15:14:00 +0100

    some more exception handling

--- viewer.server/src/one/microstream/viewer/server/InvalidRouteParameters.java
+++ viewer.server/src/one/microstream/viewer/server/InvalidRouteParameters.java
@@ -2,4 +2,9 @@ package one.microstream.viewer.server;
 
 public class InvalidRouteParameters extends RuntimeException {
 
+	public InvalidRouteParameters(final String message)
+	{
+		super(message);
+	}
+
 }
