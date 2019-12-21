583b3352a289af398ce20fe1f8e34abf0fc62f9b
TMuenz <t.muenz@xdev-software.de>
2019-12-21 11:56:15 +0100

    priv#194: Implementation and testing.

--- work-in-progress/src/one/microstream/test/corp/main/MainTestStorageAppRoot.java
+++ work-in-progress/src/one/microstream/test/corp/main/MainTestStorageAppRoot.java
@@ -6,29 +6,34 @@ import one.microstream.test.corp.logic.Test;
 import one.microstream.test.corp.logic.TestImportExport;
 
 
-public class MainTestStorageExample
+public class MainTestStorageAppRoot
 {
 	static
 	{
 //		Test.clearDefaultStorageDirectory();
 	}
 	
+	static final AppRoot APP_ROOT = new AppRoot();
+	
 	// creates and starts an embedded storage manager with all-default-settings.
-	static final EmbeddedStorageManager STORAGE = EmbeddedStorage.start();
+	static final EmbeddedStorageManager STORAGE = EmbeddedStorage.start(APP_ROOT);
 
 	public static void main(final String[] args)
 	{
 		// object graph with root either loaded on startup from an existing DB or required to be generated.
-		if(STORAGE.root() == null)
+		if(APP_ROOT.v == null)
 		{
 			// first execution enters here (database creation)
-
 			Test.print("Model data required.");
-			STORAGE.setRoot(Test.generateModelData(100));
+			APP_ROOT.set(new Value(5));
 			
 			Test.print("Storing ...");
 			STORAGE.storeRoot();
 			Test.print("Storing completed.");
+			
+			Test.print("Exporting data ...");
+			TestImportExport.testExport(STORAGE, Test.provideTimestampedDirectory("testExport"));
+			Test.print("Data export completed.");
 		}
 		else
 		{
@@ -49,5 +54,32 @@ public class MainTestStorageExample
 		// no shutdown required, the storage concept is inherently crash-safe
 		System.exit(0);
 	}
+	
+	
+	static class AppRoot
+	{
+		Value v;
 		
+		AppRoot set(final Value v)
+		{
+			this.v = v;
+			
+			return this;
+		}
+		
+	}
+	
+	static class Value
+	{
+		int v;
+
+		Value(final int v)
+		{
+			super();
+			this.v = v;
+		}
+		
+		
+	}
+	
 }
