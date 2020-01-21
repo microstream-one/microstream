b93c5531ffd39eccad973ec9684a762f8d586ce6
microstream_hg <h.grunwald@microstream.one>
2020-01-21 07:33:36 +0100

    support for names routes

--- viewer.server/src/one/microstream/viewer/server/EmbeddedStorageViewer2.java
+++ viewer.server/src/one/microstream/viewer/server/EmbeddedStorageViewer2.java
@@ -10,22 +10,29 @@ import spark.route.HttpMethod;
 public class EmbeddedStorageViewer2
 {
 	///////////////////////////////////////////////////////////////////////////
+	// constants  //
+	///////////////
+
+	private static final String DEFAULT_STORAGE_NAME = "microstream";
+
+	///////////////////////////////////////////////////////////////////////////
 	// instance fields //
 	////////////////////
 
 	private final Service sparkService;
 	private final StorageRestAdapter2 embeddedStorageRestAdapter;
+	private final String storageName;
 
 	///////////////////////////////////////////////////////////////////////////
 	// constructors //
 	/////////////////
 
 	/*
-	 * Construct with default spark Service instance
+	 * Construct with default spark service instance
 	 */
 	public EmbeddedStorageViewer2(final EmbeddedStorageManager storage)
 	{
-		this(storage, Service.ignite());
+		this(storage, Service.ignite(), DEFAULT_STORAGE_NAME);
 	}
 
 	/*
@@ -33,7 +40,24 @@ public class EmbeddedStorageViewer2
 	 */
 	public EmbeddedStorageViewer2(final EmbeddedStorageManager storage, final Service sparkService)
 	{
+		this(storage, sparkService, DEFAULT_STORAGE_NAME);
+	}
+
+	/*
+	 * Construct with custom storageName and default spark service
+	 */
+	public EmbeddedStorageViewer2(final EmbeddedStorageManager storage, final String storageName)
+	{
+		this(storage, Service.ignite(), storageName);
+	}
+
+	/*
+	 * Construct with custom spark service instance and storage name
+	 */
+	public EmbeddedStorageViewer2(final EmbeddedStorageManager storage, final Service sparkService, final String storageName)
+	{
 		super();
+		this.storageName = storageName;
 		this.sparkService = sparkService;
 		this.embeddedStorageRestAdapter = new StorageRestAdapter2(storage);
 	}
@@ -47,9 +71,9 @@ public class EmbeddedStorageViewer2
 	 */
 	public EmbeddedStorageViewer2 start()
 	{
-		this.sparkService.addRoute(HttpMethod.get, RouteImpl.create("/microstream/object/:oid",	new RouteObject2(this.embeddedStorageRestAdapter)));
-		this.sparkService.addRoute(HttpMethod.get, RouteImpl.create("/microstream/root",	   	new RouteUserRoot2(this.embeddedStorageRestAdapter)));
-		this.sparkService.addRoute(HttpMethod.get, RouteImpl.create("/microstream/dictionary",	new RouteTypeDictionary(this.embeddedStorageRestAdapter)));
+		this.sparkService.addRoute(HttpMethod.get, RouteImpl.create("/" + this.storageName + "/object/:oid",	new RouteObject2(this.embeddedStorageRestAdapter)));
+		this.sparkService.addRoute(HttpMethod.get, RouteImpl.create("/" + this.storageName + "/root",	   	new RouteUserRoot2(this.embeddedStorageRestAdapter)));
+		this.sparkService.addRoute(HttpMethod.get, RouteImpl.create("/" + this.storageName + "/dictionary",	new RouteTypeDictionary(this.embeddedStorageRestAdapter)));
 
 		this.sparkService.exception(InvalidRouteParameters.class, (e, request, response) ->
 			{
