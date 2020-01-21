2c8ebcfb0aa7c2d9146e1a9f1358314c8f731398
TMuenz <t.muenz@xdev-software.de>
2020-01-21 23:06:01 +0100

    priv#88: Iimplementation.

--- work-in-progress/src/one/microstream/test/binary_fields/BinaryHandlerTestBaseClass.java
+++ work-in-progress/src/one/microstream/test/binary_fields/BinaryHandlerTestBaseClass.java
@@ -3,23 +3,14 @@ package one.microstream.test.binary_fields;
 import one.microstream.persistence.binary.types.Binary;
 import one.microstream.persistence.types.PersistenceLoadHandler;
 
+
 public final class BinaryHandlerTestBaseClass extends AbstractBinaryHandlerTestBaseClass<TestBaseClass>
 {
-	///////////////////////////////////////////////////////////////////////////
-	// constructors //
-	/////////////////
-	
-	protected BinaryHandlerTestBaseClass()
+	public BinaryHandlerTestBaseClass()
 	{
 		super(TestBaseClass.class);
 	}
 	
-	
-	
-	///////////////////////////////////////////////////////////////////////////
-	// methods //
-	////////////
-
 	@Override
 	public final TestBaseClass create(final Binary data, final PersistenceLoadHandler handler)
 	{
