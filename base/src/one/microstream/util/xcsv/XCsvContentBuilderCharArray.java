07c6439376a81890c29a47b6ddcc3e25fbf36b0c
TMuenz <t.muenz@xdev-software.de>
2020-02-13 23:37:10 +0100

    priv#204: Added some parsing and readFile variants that were forgotten before and tiny code cleanup.

--- base/src/one/microstream/util/xcsv/XCsvContentBuilderCharArray.java
+++ base/src/one/microstream/util/xcsv/XCsvContentBuilderCharArray.java
@@ -43,7 +43,7 @@ public final class XCsvContentBuilderCharArray implements XCsvContent.Builder<_c
 	
 	public static final XCsvContentBuilderCharArray New(
 		final XCsvConfiguration csvConfiguration,
-		final XCsvDataType     dataType
+		final XCsvDataType      dataType
 	)
 	{
 		return new XCsvContentBuilderCharArray(
@@ -61,7 +61,7 @@ public final class XCsvContentBuilderCharArray implements XCsvContent.Builder<_c
 	// instance fields //
 	////////////////////
 
-	private final XCsvDataType               dataType     ;
+	private final XCsvDataType                dataType     ;
 	private final XCsvConfiguration           configuration;
 	private final Substituter<String>         stringCache  ;
 	private final XCsvParser<_charArrayRange> parser       ;
@@ -74,7 +74,7 @@ public final class XCsvContentBuilderCharArray implements XCsvContent.Builder<_c
 	/////////////////
 
 	private XCsvContentBuilderCharArray(
-		final XCsvDataType               dataType     ,
+		final XCsvDataType                dataType     ,
 		final XCsvConfiguration           configuration,
 		final Substituter<String>         stringCache  ,
 		final XCsvParser<_charArrayRange> parser       ,
