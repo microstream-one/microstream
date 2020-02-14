a71cf2989889bdc25b86e1d7932708ba6e650aa7
microstream_hg <h.grunwald@microstream.one>
2020-02-14 11:49:20 +0100

    minior improvements

--- storage.restservice.sparkjava/src/storage/restservice/sparkjava/RoutesManager.java
+++ storage.restservice.sparkjava/src/storage/restservice/sparkjava/RoutesManager.java
@@ -11,11 +11,11 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
-import one.microstream.meta.XDebug;
 import one.microstream.storage.restadapter.ViewerException;
 import spark.RouteImpl;
 import spark.Service;
 import spark.route.HttpMethod;
+
 
 public class RoutesManager
 {
@@ -38,8 +38,12 @@ public class RoutesManager
 		this.registeredRoutes = new Hashtable<>();
 		this.documentations = new Hashtable<>();
 
-		this.buildDocu();
+		this.buildLiveDocumentation();
 	}
+
+	///////////////////////////////////////////////////////////////////////////
+	// methods //
+	////////////
 
 	public void registerRoutes(final HttpMethod httpMethod, final String path, final RouteBase<?> route)
 	{
@@ -52,50 +56,56 @@ public class RoutesManager
 		methods.put(httpMethod.toString().toLowerCase(), route.getClass().getName());
 		this.sparkService.addRoute(httpMethod, RouteImpl.create(path, route));
 
-
 		methods.put(HttpMethod.options.toString().toLowerCase(), route.getClass().getName());
 		this.sparkService.addRoute(HttpMethod.options, RouteImpl.create(path, new RouteDocumentation(this)));
 	}
 
-	private void buildDocu()
+	private String getResourceFileContentAsString(final String path)
 	{
-		try(final InputStream in = this.getClass().getResourceAsStream("/resources/onlineDocu.json");
-				final BufferedReader reader = new BufferedReader(new InputStreamReader(in));)
+		try(final InputStream in = this.getClass().getResourceAsStream(path);
+			final BufferedReader reader = new BufferedReader(new InputStreamReader(in));)
+		{
+			final StringBuilder builder = new StringBuilder(in.available()*2);
+			String read = null;
+			while((read = reader.readLine()) != null)
 			{
-				final StringBuilder builder = new StringBuilder(in.available()*2);
-				String read = null;
-				while((read = reader.readLine()) != null)
-				{
-					builder.append(read);
-				}
-
-				final JsonObject docu = new Gson().fromJson(builder.toString(), JsonObject.class);
-				final JsonObject handlers = docu.getAsJsonObject("handler");
-
-				handlers.entrySet().forEach( handler -> {
-
-					Hashtable<String, JsonElement> handlerMethods = this.documentations.get(handler.getKey());
-					if(handlerMethods == null)
-					{
-						handlerMethods = new Hashtable<>();
-						this.documentations.put(handler.getKey(), handlerMethods);
-					}
-
-					final JsonObject methods =  handler.getValue().getAsJsonObject();
-
-					final Set<String> key = methods.keySet();
-					for (final String string : key)
-					{
-						handlerMethods.put(string, methods.get(string));
-					}
-				});
-
-				XDebug.println("");
+				builder.append(read);
 			}
-			catch(final Exception e )
+
+			return builder.toString();
+		}
+
+		catch(final Exception e )
+		{
+			throw new ViewerException(e.getMessage());
+		}
+	}
+
+	private void buildLiveDocumentation()
+	{
+		final String doc = this.getResourceFileContentAsString("/resources/onlineDocu.json");
+
+		final JsonObject docu = new Gson().fromJson(doc, JsonObject.class);
+		final JsonObject handlers = docu.getAsJsonObject("handler");
+
+		handlers.entrySet().forEach( handler -> {
+
+			Hashtable<String, JsonElement> handlerMethods = this.documentations.get(handler.getKey());
+			if(handlerMethods == null)
 			{
-				throw new ViewerException(e.getMessage());
+				handlerMethods = new Hashtable<>();
+				this.documentations.put(handler.getKey(), handlerMethods);
 			}
+
+			final JsonObject methods =  handler.getValue().getAsJsonObject();
+
+			final Set<String> key = methods.keySet();
+			for (final String string : key)
+			{
+				handlerMethods.put(string, methods.get(string));
+			}
+		});
+
 
 	}
 
