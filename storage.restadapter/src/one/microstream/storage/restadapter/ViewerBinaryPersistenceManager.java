7666d4db4c7cf8d57d7d63680b64ab3d10aca770
microstream_hg <h.grunwald@microstream.one>
2020-04-09 09:11:00 +0200

    add two missing override annotations

--- storage.restadapter/src/one/microstream/storage/restadapter/ViewerBinaryPersistenceManager.java
+++ storage.restadapter/src/one/microstream/storage/restadapter/ViewerBinaryPersistenceManager.java
@@ -25,10 +25,10 @@ import one.microstream.storage.types.EmbeddedStorageManager;
 public interface ViewerBinaryPersistenceManager extends PersistenceManager<Binary>
 {
 	public ObjectDescription getStorageObject(long objectId);
-	
+
 	public ObjectDescription getStorageConstant(long objectId);
-	
-	
+
+
 	public static ViewerBinaryPersistenceManager New(final EmbeddedStorageManager storage)
 	{
 		final PersistenceManager<Binary>           persistenceManager = storage.persistenceManager();
@@ -40,41 +40,41 @@ public interface ViewerBinaryPersistenceManager extends PersistenceManager<Binar
 			storage.configuration().channelCountProvider(),
 			persistenceManager.isByteOrderMismatch()
 		);
-		final PersistenceContextDispatcher<Binary> contextDispatcher  = 
+		final PersistenceContextDispatcher<Binary> contextDispatcher  =
 			PersistenceContextDispatcher.PassThrough();
 		final ViewerBinaryTypeHandlerManager       typeHandlerManager =
 			new ViewerBinaryTypeHandlerManager(persistenceManager);
-		
+
 		return new Default(
-			persistenceManager, 
-			objectRegistry, 
-			loaderCreator, 
-			contextDispatcher, 
-			typeHandlerManager, 
+			persistenceManager,
+			objectRegistry,
+			loaderCreator,
+			contextDispatcher,
+			typeHandlerManager,
 			constantRegistry
 		);
 	}
-	
-	
+
+
 	public static class Default implements ViewerBinaryPersistenceManager
 	{
 		///////////////////////////////////////////////////////////////////////////
 		// instance fields //
 		////////////////////
-		
+
 		private final PersistenceManager<Binary>           persistenceManager;
 		private final PersistenceObjectRegistry            objectRegistry    ;
 		private final PersistenceLoader.Creator<Binary>    loaderCreator     ;
 		private final PersistenceContextDispatcher<Binary> contextDispatcher ;
 		private final ViewerBinaryTypeHandlerManager       typeHandlerManager;
 		private final PersistenceObjectRegistry            constantRegistry  ;
-		
-		
-		
+
+
+
 		///////////////////////////////////////////////////////////////////////////
 		// constructors //
 		/////////////////
-		
+
 		public Default(
 			final PersistenceManager<Binary>           persistenceManager,
 			final PersistenceObjectRegistry            objectRegistry,
@@ -91,13 +91,13 @@ public interface ViewerBinaryPersistenceManager extends PersistenceManager<Binar
 			this.contextDispatcher  = contextDispatcher;
 			this.typeHandlerManager = typeHandlerManager;
 			this.constantRegistry   = constantRegistry;
-		}		
-		
-		
+		}
+
+
 		///////////////////////////////////////////////////////////////////////////
 		// methods //
 		////////////
-		
+
 		@Override
 		public final long ensureObjectId(final Object object)
 		{
@@ -113,97 +113,97 @@ public interface ViewerBinaryPersistenceManager extends PersistenceManager<Binar
 		{
 			return this.persistenceManager.ensureObjectId(object, lazyObjectIdRequestor, eagerObjectIdRequestor);
 		}
-		
+
 		@Override
 		public void consolidate()
 		{
 			this.persistenceManager.consolidate();
 		}
-		
+
 		@Override
 		public long lookupObjectId(final Object object)
 		{
 			return this.persistenceManager.lookupObjectId(object);
 		}
-		
+
 		@Override
 		public Object lookupObject(final long objectId)
 		{
 			return this.persistenceManager.lookupObject(objectId);
 		}
-		
+
 		@Override
 		public Object get()
 		{
 			return this.persistenceManager.get();
 		}
-		
+
 		@Override
 		public Object getObject(final long objectId)
 		{
 			return this.persistenceManager.getObject(objectId);
 		}
-		
+
 		@Override
 		public <C extends Consumer<Object>> C collect(final C collector, final long... objectIds)
 		{
 			return this.persistenceManager.collect(collector, objectIds);
 		}
-		
+
 		@Override
 		public long store(final Object instance)
 		{
 			return this.persistenceManager.store(instance);
 		}
-		
+
 		@Override
 		public long[] storeAll(final Object... instances)
 		{
 			return this.persistenceManager.storeAll(instances);
 		}
-		
+
 		@Override
 		public void storeAll(final Iterable<?> instances)
 		{
 			this.persistenceManager.storeAll(instances);
 		}
-		
+
 		@Override
 		public PersistenceRegisterer createRegisterer()
 		{
 			return this.persistenceManager.createRegisterer();
 		}
-		
+
 		@Override
 		public PersistenceLoader createLoader()
 		{
 			return this.persistenceManager.createLoader();
 		}
-		
+
 		@Override
 		public PersistenceStorer createLazyStorer()
 		{
 			return this.persistenceManager.createLazyStorer();
 		}
-		
+
 		@Override
 		public PersistenceStorer createStorer()
 		{
 			return this.persistenceManager.createStorer();
 		}
-		
+
 		@Override
 		public PersistenceStorer createEagerStorer()
 		{
 			return this.persistenceManager.createEagerStorer();
 		}
-	
+
 		@Override
 		public PersistenceStorer createStorer(final Creator<Binary> storerCreator)
 		{
 			return this.persistenceManager.createStorer(storerCreator);
 		}
-		
+
 		@Override
 		public void updateMetadata(
 			final PersistenceTypeDictionary typeDictionary ,
@@ -213,100 +213,102 @@ public interface ViewerBinaryPersistenceManager extends PersistenceManager<Binar
 		{
 			this.persistenceManager.updateMetadata(typeDictionary, highestTypeId, highestObjectId);
 		}
-		
+
 		@Override
 		public PersistenceObjectRegistry objectRegistry()
 		{
 			return this.persistenceManager.objectRegistry();
 		}
-		
+
 		@Override
 		public PersistenceTypeDictionary typeDictionary()
 		{
 			return this.persistenceManager.typeDictionary();
 		}
-		
+
 		@Override
 		public long currentObjectId()
 		{
 			return this.persistenceManager.currentObjectId();
 		}
-		
+
 		@Override
 		public PersistenceManager<Binary> updateCurrentObjectId(final long currentObjectId)
 		{
 			return this.persistenceManager.updateCurrentObjectId(currentObjectId);
 		}
-		
+
 		@Override
 		public PersistenceSource<Binary> source()
 		{
 			return this.persistenceManager.source();
 		}
-		
+
 		@Override
 		public PersistenceTarget<Binary> target()
 		{
 			return this.persistenceManager.target();
 		}
-		
+
 		@Override
 		public void close()
 		{
 			this.persistenceManager.close();
 		}
-		
+
 		@Override
 		public ByteOrder getTargetByteOrder()
 		{
 			return this.persistenceManager.getTargetByteOrder();
 		}
-		
+
 		@Override
 		public boolean registerLocalRegistry(final PersistenceLocalObjectIdRegistry localRegistry)
 		{
 			return this.persistenceManager.registerLocalRegistry(localRegistry);
 		}
-		
+
 		@Override
 		public void mergeEntries(final PersistenceLocalObjectIdRegistry localRegistry)
 		{
 			this.persistenceManager.mergeEntries(localRegistry);
 		}
-		
+
+		@Override
 		public ObjectDescription getStorageObject(final long objectId)
 		{
 			return (ObjectDescription)this.createViewerLoader().getObject(objectId);
 		}
-		
+
 		private PersistenceRetrieving createViewerLoader()
 		{
 			this.objectRegistry.clear();
-			
+
 			return this.loaderCreator.createLoader(
 				this.contextDispatcher.dispatchTypeHandlerLookup(this.typeHandlerManager),
 				this.contextDispatcher.dispatchObjectRegistry(this.objectRegistry),
 				this, this.persistenceManager
 			);
 		}
-		
+
+		@Override
 		public ObjectDescription getStorageConstant(final long objectId)
 		{
 			final Object object = this.constantRegistry.lookupObject(objectId);
 			final PersistenceTypeDefinition type = this.typeDictionary().lookupTypeByName(
 				object.getClass().getTypeName()
 			);
-			
+
 			final ObjectDescription objectDescription = new ObjectDescription();
-			
+
 			objectDescription.setPersistenceTypeDefinition(type);
 			objectDescription.setObjectId(objectId);
 			objectDescription.setValues(new Object[] {object});
 			objectDescription.setLength(1);
-			
+
 			return objectDescription;
 		}
-	
+
 	}
-	
+
 }
