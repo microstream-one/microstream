0f79101a4de22a446dd94ecbb9176f4109d373b1
fh <f.habermann@microstream.one>
2020-04-20 15:56:44 +0200

    JavaDoc Update: Fixed broken @linkDoc references and deprecation notes

--- persistence.binary.jdk8/src/one/microstream/jdk8/java/util/BinaryHandlerHashMapFlattened.java
+++ persistence.binary.jdk8/src/one/microstream/jdk8/java/util/BinaryHandlerHashMapFlattened.java
@@ -17,8 +17,8 @@ import one.microstream.persistence.types.PersistenceStoreHandler;
 
 /**
  * Premature prototype implementation that has to be kept for live projects using it.
- * <p>
- * Do not use! Use {@link BinaryHandlerHashMap} instead.
+ *
+ * @deprecated Do not use! Use {@link BinaryHandlerHashMap} instead.
  * 
  * @author FH
  * @author TM
