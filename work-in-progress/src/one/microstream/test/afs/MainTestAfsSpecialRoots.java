0a5a5428d5c00ba6a11ff0360e36659d3861ff49
TMuenz <t.muenz@xdev-software.de>
2020-07-30 01:22:59 +0200

    priv#358: Workaround for buggy JDK StringLolenizer.

---
+++ work-in-progress/src/one/microstream/test/afs/MainTestAfsSpecialRoots.java
@@ -0,0 +1,23 @@
+package one.microstream.test.afs;
+
+import java.nio.file.Path;
+
+import one.microstream.afs.AFile;
+import one.microstream.afs.nio.NioFileSystem;
+import one.microstream.io.XIO;
+
+public class MainTestAfsSpecialRoots
+{
+	public static void main(final String[] args)
+	{
+		final NioFileSystem fs = NioFileSystem.get();
+		
+		final Path filePath = XIO.Path("/test/bla/blub/stuff.lol");
+		System.out.println(filePath);
+		
+		final AFile file = fs.ensureFile(filePath);
+		
+		// must replicate the path string, including the leading "/".
+		System.out.println(file);
+	}
+}
