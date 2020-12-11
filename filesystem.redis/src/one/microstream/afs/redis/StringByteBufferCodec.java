8a536df9e0cc8ba7821e875cef9801b35630fd9f
FH <f.habermann@microstream.one>
2020-12-11 16:08:13 +0100

    Fix for priv#463

--- filesystem.redis/src/one/microstream/afs/redis/StringByteBufferCodec.java
+++ filesystem.redis/src/one/microstream/afs/redis/StringByteBufferCodec.java
@@ -4,8 +4,10 @@ import java.nio.ByteBuffer;
 
 import io.lettuce.core.codec.RedisCodec;
 import io.lettuce.core.codec.StringCodec;
+import io.lettuce.core.codec.ToByteBufEncoder;
+import io.netty.buffer.ByteBuf;
 
-public interface StringByteBufferCodec extends RedisCodec<String, ByteBuffer>
+public interface StringByteBufferCodec extends RedisCodec<String, ByteBuffer>, ToByteBufEncoder<String, ByteBuffer>
 {
 
 	public static StringByteBufferCodec New()
@@ -20,12 +22,35 @@ public interface StringByteBufferCodec extends RedisCodec<String, ByteBuffer>
 		{
 			super();
 		}
+		
+		private ByteBuffer copy(
+			final ByteBuffer buffer
+		)
+		{
+			final int        pos  = buffer.position();
+			final ByteBuffer copy = ByteBuffer.allocateDirect(buffer.limit());
+	        
+	        buffer.rewind();
+
+	        copy.order(buffer.order());
+	        copy.put(buffer);
+	        copy.position(pos);
+
+	        buffer.position(pos);
+
+	        return copy;
+	    }
 
 		@Override
 		public ByteBuffer encodeKey(
 			final String key
 		)
 		{
+			if(key == null)
+			{
+				return ByteBuffer.allocate(0);
+			}
+			
 			return StringCodec.UTF8.encodeKey(key);
 		}
 
@@ -42,7 +67,12 @@ public interface StringByteBufferCodec extends RedisCodec<String, ByteBuffer>
 			final ByteBuffer value
 		)
 		{
-			return value;
+			if(value == null)
+			{
+				return ByteBuffer.allocate(0);
+			}
+			
+			return this.copy(value);
 		}
 
 		@Override
@@ -50,7 +80,54 @@ public interface StringByteBufferCodec extends RedisCodec<String, ByteBuffer>
 			final ByteBuffer bytes
 		)
 		{
-			return bytes;
+			if(bytes == null)
+			{
+				return ByteBuffer.allocate(0);
+			}
+			
+			return this.copy(bytes);
+		}
+
+		@Override
+		public void encodeKey(
+			final String  key   ,
+			final ByteBuf target
+		)
+		{
+			if(key != null)
+			{
+				StringCodec.UTF8.encodeKey(key, target);
+			}
+		}
+
+		@Override
+		public void encodeValue(
+			final ByteBuffer value ,
+			final ByteBuf    target
+		)
+		{
+			if(value != null)
+			{
+				target.writeBytes(value);
+				value.flip();
+			}
+		}
+
+		@Override
+		public int estimateSize(
+			final Object keyOrValue
+		)
+		{
+			if(keyOrValue instanceof String)
+			{
+				return StringCodec.UTF8.estimateSize(keyOrValue);
+			}
+			if(keyOrValue instanceof ByteBuffer)
+			{
+				return ((ByteBuffer)keyOrValue).limit();
+			}
+			
+			return 0;
 		}
 
 	}
