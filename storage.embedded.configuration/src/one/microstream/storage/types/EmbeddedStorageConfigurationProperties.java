b91e32582c23e9a4e613aee2de908ed921a93212
FH <f.habermann@microstream.one>
2021-02-01 16:44:01 +0100

    priv#461: implementation

---
+++ storage.embedded.configuration/src/one/microstream/storage/types/EmbeddedStorageConfigurationProperties.java
@@ -0,0 +1,115 @@
+
+package one.microstream.storage.types;
+
+/**
+ * All supported properties for external configuration files.
+ *
+ */
+public interface EmbeddedStorageConfigurationProperties
+{
+	/**
+	 * @see EmbeddedStorageConfigurationBuilder#setStorageDirectory(String)
+	 */
+	public final static String STORAGE_DIRECTORY             = "storageDirectory";
+
+	/**
+	 * @see EmbeddedStorageConfigurationBuilder#setDeletionDirectory(String)
+	 */
+	public final static String DELETION_DIRECTORY            = "deletionDirectory";
+
+	/**
+	 * @see EmbeddedStorageConfigurationBuilder#setTruncationDirectory(String)
+	 */
+	public final static String TRUNCATION_DIRECTORY          = "truncationDirectory";
+
+	/**
+	 * @see EmbeddedStorageConfigurationBuilder#setBackupDirectory(String)
+	 */
+	public final static String BACKUP_DIRECTORY              = "backupDirectory";
+
+	/**
+	 * @see EmbeddedStorageConfigurationBuilder#setChannelCount(int)
+	 */
+	public final static String CHANNEL_COUNT                 = "channelCount";
+
+	/**
+	 * @see EmbeddedStorageConfigurationBuilder#setChannelDirectoryPrefix(String)
+	 */
+	public final static String CHANNEL_DIRECTORY_PREFIX      = "channelDirectoryPrefix";
+
+	/**
+	 * @see EmbeddedStorageConfigurationBuilder#setDataFilePrefix(String)
+	 */
+	public final static String DATA_FILE_PREFIX              = "dataFilePrefix";
+
+	/**
+	 * @see EmbeddedStorageConfigurationBuilder#setDataFileSuffix(String)
+	 */
+	public final static String DATA_FILE_SUFFIX              = "dataFileSuffix";
+
+	/**
+	 * @see EmbeddedStorageConfigurationBuilder#setTransactionFilePrefix(String)
+	 */
+	public final static String TRANSACTION_FILE_PREFIX       = "transactionFilePrefix";
+
+	/**
+	 * @see EmbeddedStorageConfigurationBuilder#setTransactionFileSuffix(String)
+	 */
+	public final static String TRANSACTION_FILE_SUFFIX       = "transactionFileSuffix";
+
+	/**
+	 * @see EmbeddedStorageConfigurationBuilder#setTypeDictionaryFilename(String)
+	 */
+	public final static String TYPE_DICTIONARY_FILENAME      = "typeDictionaryFilename";
+
+	/**
+	 * @see EmbeddedStorageConfigurationBuilder#setRescuedFileSuffix(String)
+	 */
+	public final static String RESCUED_FILE_SUFFIX           = "rescuedFileSuffix";
+
+	/**
+	 * @see EmbeddedStorageConfigurationBuilder#setLockFileName(String)
+	 */
+	public final static String LOCK_FILE_NAME                = "lockFileName";
+
+	/**
+	 * @see EmbeddedStorageConfigurationBuilder#setHousekeepingIntervalMs(long)
+	 */
+	public final static String HOUSEKEEPING_INTERVAL_MS      = "housekeepingIntervalMs";
+
+	/**
+	 * @see EmbeddedStorageConfigurationBuilder#setHousekeepingTimeBudgetNs(long)
+	 */
+	public final static String HOUSEKEEPING_TIME_BUDGET_NS   = "housekeepingTimeBudgetNs";
+
+	/**
+	 * @see EmbeddedStorageConfigurationBuilder#setEntityCacheThreshold(long)
+	 */
+	public final static String ENTITY_CACHE_THRESHOLD        = "entityCacheThreshold";
+
+	/**
+	 * @see EmbeddedStorageConfigurationBuilder#setEntityCacheTimeout(long)
+	 */
+	public final static String ENTITY_CACHE_TIMEOUT_MS       = "entityCacheTimeoutMs";
+
+	/**
+	 * @see EmbeddedStorageConfigurationBuilder#setDataFileMinimumSize(int)
+	 */
+	public final static String DATA_FILE_MINIMUM_SIZE        = "dataFileMinimumSize";
+
+	/**
+	 * @see EmbeddedStorageConfigurationBuilder#setDataFileMaximumSize(int)
+	 */
+	public final static String DATA_FILE_MAXIMUM_SIZE        = "dataFileMaximumSize";
+
+	/**
+	 * @see EmbeddedStorageConfigurationBuilder#setDataFileDissolveRatio(double)
+	 */
+	public final static String DATA_FILE_MINIMUM_USE_RATIO   = "dataFileMinimumUseRatio";
+
+	/**
+	 * @see EmbeddedStorageConfigurationBuilder#setDataFileCleanupHeadFile(boolean)
+	 */
+	public final static String DATA_FILE_CLEANUP_HEAD_FILE   = "dataFileCleanupHeadFile";
+
+}
---
+++ storage.embedded.configuration/src/one/microstream/storage/types/EmbeddedStorageFoundationCreatorConfigurationBased.java
@@ -0,0 +1,152 @@
+package one.microstream.storage.types;
+
+import static one.microstream.X.notNull;
+
+import one.microstream.afs.ADirectory;
+import one.microstream.afs.nio.NioFileSystem;
+import one.microstream.chars.XChars;
+import one.microstream.configuration.types.Configuration;
+
+public interface EmbeddedStorageFoundationCreatorConfigurationBased extends EmbeddedStorageFoundation.Creator
+{
+	public static EmbeddedStorageFoundationCreatorConfigurationBased New(
+		final Configuration configuration
+	)
+	{
+		return new EmbeddedStorageFoundationCreatorConfigurationBased.Default(
+			notNull(configuration)
+		);
+	}
+	
+	public static class Default implements
+	EmbeddedStorageFoundationCreatorConfigurationBased,
+	EmbeddedStorageConfigurationProperties
+	{
+		private final Configuration configuration;
+
+		Default(
+			final Configuration configuration
+		)
+		{
+			super();
+			this.configuration = configuration;
+		}
+
+		@Override
+		public EmbeddedStorageFoundation<?> createEmbeddedStorageFoundation()
+		{
+			final StorageConfiguration.Builder<?> configBuilder = Storage.ConfigurationBuilder()
+				.setStorageFileProvider   (this.createFileProvider()          )
+				.setChannelCountProvider  (this.createChannelCountProvider()  )
+				.setHousekeepingController(this.createHousekeepingController())
+				.setDataFileEvaluator     (this.createDataFileEvaluator()     )
+				.setEntityCacheEvaluator  (this.createEntityCacheEvaluator()  )
+			;
+
+			this.configuration.opt(BACKUP_DIRECTORY)
+				.filter(backupDirectory -> !XChars.isEmpty(backupDirectory))
+				.ifPresent(backupDirectory -> configBuilder.setBackupSetup(
+					Storage.BackupSetup(backupDirectory)
+				))
+			;
+
+			return EmbeddedStorage.Foundation(
+				configBuilder.createConfiguration()
+			);
+		}
+
+		private StorageLiveFileProvider createFileProvider()
+		{
+			final NioFileSystem fileSystem = NioFileSystem.New();
+			
+			final ADirectory baseDirectory = fileSystem.ensureDirectoryPath(
+				this.configuration.opt(STORAGE_DIRECTORY)
+					.orElse(StorageLiveFileProvider.Defaults.defaultStorageDirectory())
+			);
+			
+			final StorageFileNameProvider fileNameProvider = StorageFileNameProvider.New(
+				this.configuration.opt(CHANNEL_DIRECTORY_PREFIX)
+					.orElse(StorageFileNameProvider.Defaults.defaultChannelDirectoryPrefix()),
+				this.configuration.opt(DATA_FILE_PREFIX)
+					.orElse(StorageFileNameProvider.Defaults.defaultDataFilePrefix()),
+				this.configuration.opt(DATA_FILE_SUFFIX)
+					.orElse(StorageFileNameProvider.Defaults.defaultDataFileSuffix()),
+				this.configuration.opt(TRANSACTION_FILE_PREFIX)
+					.orElse(StorageFileNameProvider.Defaults.defaultTransactionsFilePrefix()),
+				this.configuration.opt(TRANSACTION_FILE_SUFFIX)
+					.orElse(StorageFileNameProvider.Defaults.defaultTransactionsFileSuffix()),
+				this.configuration.opt(RESCUED_FILE_SUFFIX)
+					.orElse(StorageFileNameProvider.Defaults.defaultRescuedFileSuffix()),
+				this.configuration.opt(TYPE_DICTIONARY_FILENAME)
+					.orElse(StorageFileNameProvider.Defaults.defaultTypeDictionaryFileName()),
+				this.configuration.opt(LOCK_FILE_NAME)
+					.orElse(StorageFileNameProvider.Defaults.defaultLockFileName())
+			);
+			
+			final StorageLiveFileProvider.Builder<?> builder = Storage.FileProviderBuilder(fileSystem)
+				.setDirectory(baseDirectory)
+				.setFileNameProvider(fileNameProvider)
+			;
+			
+			this.configuration.opt(DELETION_DIRECTORY)
+				.filter(deletionDirectory -> !XChars.isEmpty(deletionDirectory))
+				.ifPresent(deletionDirectory -> builder.setDeletionDirectory(
+					fileSystem.ensureDirectoryPath(deletionDirectory)
+				))
+			;
+			
+			this.configuration.opt(TRUNCATION_DIRECTORY)
+				.filter(truncationDirectory -> !XChars.isEmpty(truncationDirectory))
+				.ifPresent(truncationDirectory -> builder.setTruncationDirectory(
+					fileSystem.ensureDirectoryPath(truncationDirectory)
+				))
+			;
+			
+			return builder.createFileProvider();
+		}
+
+		protected StorageChannelCountProvider createChannelCountProvider()
+		{
+			return Storage.ChannelCountProvider(
+				this.configuration.optInteger(CHANNEL_COUNT)
+					.orElse(StorageChannelCountProvider.Defaults.defaultChannelCount())
+			);
+		}
+
+		private StorageHousekeepingController createHousekeepingController()
+		{
+			return Storage.HousekeepingController(
+				this.configuration.optLong(HOUSEKEEPING_INTERVAL_MS)
+					.orElse(StorageHousekeepingController.Defaults.defaultHousekeepingIntervalMs()),
+				this.configuration.optLong(HOUSEKEEPING_TIME_BUDGET_NS)
+					.orElse(StorageHousekeepingController.Defaults.defaultHousekeepingTimeBudgetNs())
+			);
+		}
+
+		private StorageDataFileEvaluator createDataFileEvaluator()
+		{
+			return Storage.DataFileEvaluator(
+				this.configuration.optInteger(DATA_FILE_MINIMUM_SIZE)
+					.orElse(StorageDataFileEvaluator.Defaults.defaultFileMinimumSize()),
+				this.configuration.optInteger(DATA_FILE_MAXIMUM_SIZE)
+					.orElse(StorageDataFileEvaluator.Defaults.defaultFileMaximumSize()),
+				this.configuration.optDouble(DATA_FILE_MINIMUM_USE_RATIO)
+					.orElse(StorageDataFileEvaluator.Defaults.defaultMinimumUseRatio()),
+				this.configuration.optBoolean(DATA_FILE_CLEANUP_HEAD_FILE)
+					.orElse(StorageDataFileEvaluator.Defaults.defaultResolveHeadfile())
+			);
+		}
+
+		private StorageEntityCacheEvaluator createEntityCacheEvaluator()
+		{
+			return Storage.EntityCacheEvaluator(
+				this.configuration.optLong(ENTITY_CACHE_TIMEOUT_MS)
+					.orElse(StorageEntityCacheEvaluator.Defaults.defaultTimeoutMs()),
+				this.configuration.optLong(ENTITY_CACHE_THRESHOLD)
+					.orElse(StorageEntityCacheEvaluator.Defaults.defaultCacheThreshold())
+			);
+		}
+		
+	}
+	
+}
