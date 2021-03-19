238138b452b19a57071830c5dbe4c6793a487622
fh-ms <f.habermann@microstream.one>
2021-03-19 11:15:28 +0100

    priv#504: fixed legacy type mapping regression introduced with enum handling

--- persistence.binary/src/one/microstream/persistence/binary/types/BinaryLegacyTypeHandlerGenericEnumMapped.java
+++ persistence.binary/src/one/microstream/persistence/binary/types/BinaryLegacyTypeHandlerGenericEnumMapped.java
@@ -3,12 +3,13 @@ package one.microstream.persistence.binary.types;
 import static one.microstream.X.mayNull;
 import static one.microstream.X.notNull;
 
-import one.microstream.collections.types.XGettingTable;
+import one.microstream.collections.types.XGettingEnum;
 import one.microstream.persistence.types.PersistenceLegacyTypeHandler;
 import one.microstream.persistence.types.PersistenceLegacyTypeHandlingListener;
 import one.microstream.persistence.types.PersistenceLoadHandler;
 import one.microstream.persistence.types.PersistenceTypeDefinition;
 import one.microstream.persistence.types.PersistenceTypeHandler;
+import one.microstream.typing.KeyValue;
 
 
 public class BinaryLegacyTypeHandlerGenericEnumMapped<T>
@@ -17,14 +18,14 @@ extends BinaryLegacyTypeHandlerGenericEnum<T>
 	///////////////////////////////////////////////////////////////////////////
 	// static methods //
 	///////////////////
-	
+
 	public static <T> BinaryLegacyTypeHandlerGenericEnumMapped<T> New(
-		final PersistenceTypeDefinition                     typeDefinition              ,
-		final PersistenceTypeHandler<Binary, T>             typeHandler                 ,
-		final XGettingTable<Long, BinaryValueSetter>        translatorsWithTargetOffsets,
-		final Integer[]                                     ordinalMapping              ,
-		final PersistenceLegacyTypeHandlingListener<Binary> listener                    ,
-		final boolean                                       switchByteOrder
+		final PersistenceTypeDefinition                       typeDefinition              ,
+		final PersistenceTypeHandler<Binary, T>               typeHandler                 ,
+		final XGettingEnum<KeyValue<Long, BinaryValueSetter>> translatorsWithTargetOffsets,
+		final Integer[]                                       ordinalMapping              ,
+		final PersistenceLegacyTypeHandlingListener<Binary>   listener                    ,
+		final boolean                                         switchByteOrder
 	)
 	{
 		return new BinaryLegacyTypeHandlerGenericEnumMapped<>(
@@ -37,21 +38,21 @@ extends BinaryLegacyTypeHandlerGenericEnum<T>
 			switchByteOrder
 		);
 	}
-	
-	
-	
+
+
+
 	///////////////////////////////////////////////////////////////////////////
 	// instance fields //
 	////////////////////
-	
+
 	private final Integer[] ordinalMapping;
-	
-	
-	
+
+
+
 	///////////////////////////////////////////////////////////////////////////
 	// constructors //
 	/////////////////
-	
+
 	BinaryLegacyTypeHandlerGenericEnumMapped(
 		final PersistenceTypeDefinition                     typeDefinition  ,
 		final PersistenceTypeHandler<Binary, T>             typeHandler     ,
@@ -65,24 +66,24 @@ extends BinaryLegacyTypeHandlerGenericEnum<T>
 		super(typeDefinition, typeHandler, valueTranslators, targetOffsets, listener, switchByteOrder);
 		this.ordinalMapping = ordinalMapping;
 	}
-	
-	
-	
+
+
+
 	///////////////////////////////////////////////////////////////////////////
 	// methods //
 	////////////
-		
+
 	@Override
 	protected T internalCreate(final Binary data, final PersistenceLoadHandler handler)
 	{
 		return PersistenceLegacyTypeHandler.resolveEnumConstant(this, data, this.ordinalMapping);
 	}
-	
+
 	@Override
 	public void updateState(final Binary rawData, final T instance, final PersistenceLoadHandler handler)
 	{
 		// debug hook
 		super.updateState(rawData, instance, handler);
 	}
-	
+
 }
