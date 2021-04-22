6e2c39ee969b9fc2cc2a64b91fca95b06568afc8
fh-ms <f.habermann@microstream.one>
2021-04-22 14:53:35 +0200

    Add configuration parameter to SqlDataSourceProvider

--- afs/sql/src/main/java/one/microstream/afs/sql/types/SqlDataSourceProvider.java
+++ afs/sql/src/main/java/one/microstream/afs/sql/types/SqlDataSourceProvider.java
@@ -2,8 +2,10 @@ package one.microstream.afs.sql.types;
 
 import javax.sql.DataSource;
 
+import one.microstream.configuration.types.Configuration;
+
 @FunctionalInterface
 public interface SqlDataSourceProvider
 {
-	public DataSource provideDataSource();
+	public DataSource provideDataSource(Configuration configuration);
 }
