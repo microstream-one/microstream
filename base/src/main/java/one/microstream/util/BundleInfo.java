d91f0b95640601cfa6210679d77b1623cbcec19a
fh-ms <f.habermann@microstream.one>
2021-07-13 09:39:31 +0200

    Add bundle info doc

--- base/src/main/java/one/microstream/util/BundleInfo.java
+++ base/src/main/java/one/microstream/util/BundleInfo.java
@@ -1,5 +1,25 @@
 package one.microstream.util;
 
+/*-
+ * #%L
+ * microstream-base
+ * %%
+ * Copyright (C) 2019 - 2021 MicroStream Software
+ * %%
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ * 
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License, v. 2.0 are satisfied: GNU General Public License, version 2
+ * with the GNU Classpath Exception which is
+ * available at https://www.gnu.org/software/classpath/license.html.
+ * 
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ * #L%
+ */
+
 import static one.microstream.chars.XChars.notEmpty;
 
 import java.io.IOException;
@@ -41,6 +61,11 @@ public interface BundleInfo
 		return Load("microstream-base");
 	}
 	
+	/**
+	 * 
+	 * @param bundleName the name of the bundle, in Maven environments the artifact id
+	 * @return
+	 */
 	public static BundleInfo Load(final String bundleName)
 	{
 		return new Provider.ManifestAndMavenBased(
