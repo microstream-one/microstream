fec73b964d8351b98c0398e38a5137ca62762472
fh-ms <f.habermann@microstream.one>
2021-12-22 10:21:39 +0100

    Add javadoc

--- base/src/main/java/one/microstream/util/logging/Logging.java
+++ base/src/main/java/one/microstream/util/logging/Logging.java
@@ -1,12 +1,8 @@
 package one.microstream.util.logging;
 
-import static one.microstream.X.notNull;
-
-import java.util.Iterator;
-
 /*-
  * #%L
- * microstream-persistence-binary
+ * microstream-base
  * %%
  * Copyright (C) 2019 - 2021 MicroStream Software
  * %%
@@ -24,6 +20,10 @@ import java.util.Iterator;
  * #L%
  */
 
+import static one.microstream.X.notNull;
+
+import java.util.Iterator;
+import java.util.List;
 import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.function.ToIntFunction;
@@ -35,8 +35,13 @@ import one.microstream.X;
 import one.microstream.chars.VarString;
 import one.microstream.collections.EqHashTable;
 import one.microstream.math.XMath;
+import one.microstream.typing.KeyValue;
 import one.microstream.util.BundleInfo;
 
+/**
+ * Static utility collection for various logging purposes.
+ * 
+ */
 public final class Logging
 {
 	private final static EqHashTable<Object, Function<Object, String>> toStringConverters = EqHashTable.New();
@@ -46,7 +51,7 @@ public final class Logging
 		try
 		{
 			getLogger(Logging.class).info(
-				"MicroStream Version {}",
+				"MicroStream Version {}"       ,
 				BundleInfo.LoadBase().version()
 			);
 		}
@@ -59,16 +64,34 @@ public final class Logging
 		}
 	}
 	
+	/**
+	 * Get the logging facade for a specific class.
+	 * 
+	 * @param clazz the requesting context
+	 * @return the logging facade
+	 */
 	public static Logger getLogger(final Class<?> clazz)
 	{
 		return LoggerFactory.getLogger(clazz);
 	}
 	
+	/**
+	 * The default Object to String converter, which returns an empty String.
+	 * 
+	 * @return Object to String converter which returns an empty String
+	 */
 	private static Function<Object, String> DefaultToStringConverter()
 	{
 		return obj -> "";
 	}
 	
+	/**
+	 * Gets the Object to String converter for a specific context,
+	 * or the {@link #DefaultToStringConverter()} if none was specified before.
+	 * 
+	 * @param context the requesting context
+	 * @return an Object to String converter
+	 */
 	public static Function<Object, String> getToStringConverter(final Object context)
 	{
 		final Function<Object, String> function = toStringConverters.get(context);
@@ -78,42 +101,94 @@ public final class Logging
 		;
 	}
 	
+	/**
+	 * Registers an Object to String converter for a specific context.
+	 * 
+	 * @param context   the context to register the converter for
+	 * @param converter the Object to String converter
+	 * @return the old converter which was registered before, or null
+	 */
 	public static Function<Object, String> setToStringConverter(
 		final Object                   context  ,
 		final Function<Object, String> converter
 	)
 	{
-		toStringConverters.put(context, converter);
-		return converter;
+		final KeyValue<Object, Function<Object, String>> old = toStringConverters.putGet(
+			notNull(context  ),
+			notNull(converter)
+		);
+		return old != null
+			? old.value()
+			: null
+		;
 	}
 	
+	/**
+	 * Creates a new lazy argument for a specific context, whose Object to String converter will be used.
+	 * The lazy args' {@link #toString()} method is only called on demand.
+	 * 
+	 * @param context  the context to register the lazy arg for
+	 * @param argument the argument
+	 * @return the lazy object
+	 */
 	public static final Object LazyArgInContext(
-		final Object context,
-		final Object object
+		final Object context ,
+		final Object argument
 	)
 	{
-		return LazyArgInContext(context, () -> object);
+		return LazyArgInContext(context, () -> argument);
 	}
 	
+	/**
+	 * Creates a new lazy argument for a specific context, whose Object to String converter will be used.
+	 * The lazy args' {@link #toString()} method is only called on demand.
+	 * 
+	 * @param context  the context to register the lazy arg for
+	 * @param supplier the argument supplier
+	 * @return the lazy object
+	 */
 	public static final Object LazyArgInContext(
 		final Object      context ,
 		final Supplier<?> supplier
 	)
 	{
+		notNull(context );
+		notNull(supplier);
+		
 		return new Object()
 		{
 			@Override
 			public String toString()
 			{
-				return getToStringConverter(context).apply(
-					supplier.get()
-				);
+				return getToStringConverter(context).apply(supplier.get())
+				;
 			}
 		};
 	}
+	
+	/**
+	 * Creates a lazy argument.
+	 * The lazy args' {@link #toString()} method is only called on demand.
+	 * 
+	 * @param argument the argument
+	 * @return the lazy argument
+	 */
+	public static final Object LazyArg(final Object argument)
+	{
+		return LazyArg(() -> argument);
+	}
 
+	/**
+	 * Creates a lazy argument.
+	 * The lazy args' {@link #toString()} method is only called on demand.
+	 * 
+	 * @param supplier the argument supplier
+	 * @return the lazy object
+	 */
 	public static final Object LazyArg(final Supplier<?> supplier)
 	{
+		notNull(supplier);
+		
 		return new Object()
 		{
 			@Override
@@ -124,11 +199,41 @@ public final class Logging
 		};
 	}
 
+	/**
+	 * Creates a Object to String converter, which only includes a limited amount of elements
+	 * in the resulting String, if {@link Iterable}s or arrays are to be converted.
+	 * <p>
+	 * For all other object's {@link String#valueOf(Object)} is used as converter.
+	 * <p>
+	 * For example, a {@link List} of {@link Integer}s from 1 to 100, and a limit of 3,
+	 * will result in a {@link String} like this:<br>
+	 * <code>"[1, 2, 3, ...]"</code>
+	 * 
+	 * @param limit maximum amount of converted elements in {@link Iterable}s and arrays
+	 * @return the new Object to String converter
+	 */
 	public static Function<Object, String> LimitedElementsToStringConverter(final int limit)
 	{
-		return LimitedElementsToStringConverter(limit, String::valueOf);
+		return LimitedElementsToStringConverter(
+			limit          ,
+			String::valueOf
+		);
 	}
 		
+	/**
+	 * Creates a Object to String converter, which only includes a limited amount of elements
+	 * in the resulting String, if {@link Iterable}s or arrays are to be converted.
+	 * <p>
+	 * For all other object's the supplied <code>toStringConverter</code> will be used.
+	 * <p>
+	 * For example, a {@link List} of {@link Integer}s from 1 to 100, and a limit of 3,
+	 * will result in a {@link String} like this:<br>
+	 * <code>"[1, 2, 3, ...]"</code>
+	 * 
+	 * @param limit maximum amount of converted elements in {@link Iterable}s and arrays
+	 * @param toStringConverter the converter for all other objects
+	 * @return the new Object to String converter
+	 */
 	public static Function<Object, String> LimitedElementsToStringConverter(
 		final int                      limit            ,
 		final Function<Object, String> toStringConverter
@@ -140,6 +245,20 @@ public final class Logging
 		);
 	}
 	
+	/**
+	 * Creates a Object to String converter, which only includes a limited amount of elements
+	 * in the resulting String, if {@link Iterable}s or arrays are to be converted.
+	 * <p>
+	 * For all other object's the supplied <code>toStringConverter</code> will be used.
+	 * <p>
+	 * For example, a {@link List} of {@link Integer}s from 1 to 100, and a limit of 3,
+	 * will result in a {@link String} like this:<br>
+	 * <code>"[1, 2, 3, ...]"</code>
+	 * 
+	 * @param limitProvider provides maximum amount of converted elements in {@link Iterable}s and arrays
+	 * @param toStringConverter the converter for all other objects
+	 * @return the new Object to String converter
+	 */
 	public static Function<Object, String> LimitedElementsToStringConverter(
 		final ToIntFunction<Object>    limitProvider    ,
 		final Function<Object, String> toStringConverter
@@ -152,7 +271,7 @@ public final class Logging
 	}
 	
 	
-	static class LimitedElementsToStringConverter implements Function<Object, String>
+	private static class LimitedElementsToStringConverter implements Function<Object, String>
 	{
 		private final ToIntFunction<Object>    limitProvider    ;
 		private final Function<Object, String> toStringConverter;
@@ -225,4 +344,5 @@ public final class Logging
 		// static only
 		throw new UnsupportedOperationException();
 	}
+	
 }
