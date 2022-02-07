cf007327f2f8c8b05366f54d4a8bf20ae08e833b
Zdenek Jonas <johny2000uwb@gmail.com>
2022-02-07 06:34:38 +0100

    update vulnerable depedencies + licence format of MicrostreamConfiguration (#307)

--- integrations/spring-boot/src/main/java/one/microstream/integrations/spring/boot/types/MicrostreamConfiguration.java
+++ integrations/spring-boot/src/main/java/one/microstream/integrations/spring/boot/types/MicrostreamConfiguration.java
@@ -1,5 +1,25 @@
 package one.microstream.integrations.spring.boot.types;
 
+/*-
+ * #%L
+ * microstream-spring
+ * %%
+ * Copyright (C) 2019 - 2021 MicroStream Software
+ * %%
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License, v. 2.0 are satisfied: GNU General Public License, version 2
+ * with the GNU Classpath Exception which is
+ * available at https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ * #L%
+ */
+
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -16,26 +36,6 @@ import org.springframework.core.env.AbstractEnvironment;
 import org.springframework.core.env.EnumerablePropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.core.env.MutablePropertySources;
-
-/*-
- * #%L
- * microstream-spring
- * %%
- * Copyright (C) 2019 - 2021 MicroStream Software
- * %%
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License 2.0 which is available at
- * http://www.eclipse.org/legal/epl-2.0.
- * 
- * This Source Code may also be made available under the following Secondary
- * Licenses when the conditions for such availability set forth in the Eclipse
- * Public License, v. 2.0 are satisfied: GNU General Public License, version 2
- * with the GNU Classpath Exception which is
- * available at https://www.gnu.org/software/classpath/license.html.
- * 
- * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
- * #L%
- */
 
 import one.microstream.reflect.ClassLoaderProvider;
 import one.microstream.storage.embedded.configuration.types.EmbeddedStorageConfigurationBuilder;
