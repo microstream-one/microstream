1643db7d59fa43d547cd34c0b2d337a60ab3f488
Zdenek Jonas <johny2000uwb@gmail.com>
2022-03-18 13:07:25 +0100

    update licence year from 2021 to 2022

--- examples/custom-legacy-type-handler/src/main/java/one/microstream/examples/customlegacytypehandler/NicePlace.java
+++ examples/custom-legacy-type-handler/src/main/java/one/microstream/examples/customlegacytypehandler/NicePlace.java
@@ -4,7 +4,7 @@ package one.microstream.examples.customlegacytypehandler;
  * #%L
  * microstream-examples-custom-legacy-type-handler
  * %%
- * Copyright (C) 2019 - 2021 MicroStream Software
+ * Copyright (C) 2019 - 2022 MicroStream Software
  * %%
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
