7fad3f0147eea0f800a30af8a5c6d4cb5da0eeb9
fh-ms <f.habermann@microstream.one>
2022-08-09 09:47:04 +0200

    Fix comments

--- base/src/main/java/one/microstream/collections/Set_long.java
+++ base/src/main/java/one/microstream/collections/Set_long.java
@@ -1,5 +1,25 @@
 package one.microstream.collections;
 
+/*-
+ * #%L
+ * MicroStream Base
+ * %%
+ * Copyright (C) 2019 - 2022 MicroStream Software
+ * %%
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ * 
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License, v. 2.0 are satisfied: GNU General Public License, version 2
+ * with the GNU Classpath Exception which is
+ * available at https://www.gnu.org/software/classpath/license.html.
+ * 
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ * #L%
+ */
+
 import one.microstream.collections.interfaces.OptimizableCollection;
 import one.microstream.functional._longIterable;
 import one.microstream.functional._longPredicate;
@@ -7,11 +27,6 @@ import one.microstream.functional._longProcedure;
 import one.microstream.math.XMath;
 import one.microstream.typing.Composition;
 
-/**
- *
- * 
- *
- */
 public interface Set_long extends OptimizableCollection, Composition, _longIterable
 {
 	public boolean add(long element);
