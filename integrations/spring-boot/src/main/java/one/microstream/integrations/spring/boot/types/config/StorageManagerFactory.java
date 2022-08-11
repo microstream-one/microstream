b876ebb8cdde530889aa7476c31209ddc9ca4ceb
Zdenek Jonas <johny2000uwb@gmail.com>
2022-08-11 07:30:59 +0200

    fix compile error

--- integrations/spring-boot/src/main/java/one/microstream/integrations/spring/boot/types/config/StorageManagerFactory.java
+++ integrations/spring-boot/src/main/java/one/microstream/integrations/spring/boot/types/config/StorageManagerFactory.java
@@ -37,6 +37,7 @@ import org.springframework.core.env.Environment;
 import org.springframework.core.env.MutablePropertySources;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
