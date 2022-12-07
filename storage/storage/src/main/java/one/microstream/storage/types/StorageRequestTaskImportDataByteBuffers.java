4133bebb507fae20078ccf91de490ab82203e24a
fh-ms <f.habermann@microstream.one>
2022-12-07 09:50:24 +0100

    Update header

--- storage/storage/src/main/java/one/microstream/storage/types/StorageRequestTaskImportDataByteBuffers.java
+++ storage/storage/src/main/java/one/microstream/storage/types/StorageRequestTaskImportDataByteBuffers.java
@@ -4,7 +4,7 @@ package one.microstream.storage.types;
  * #%L
  * microstream-storage
  * %%
- * Copyright (C) 2019 - 2021 MicroStream Software
+ * Copyright (C) 2019 - 2022 MicroStream Software
  * %%
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
