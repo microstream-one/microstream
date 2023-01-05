566980523be9483c9a898ea5f4065e3cdee0d7f1
Rudy De Busscher <r.d.busscher@microstream.one>
2023-01-05 10:12:25 +0100

    Support Qualifier on EmbeddedStorageFoundationCustomizer and StorageManagerInitializer.

--- integrations/spring-boot/src/it/multi-manager/src/main/java/test/microstream/integrations/spring/boot/service/ProductService.java
+++ integrations/spring-boot/src/it/multi-manager/src/main/java/test/microstream/integrations/spring/boot/service/ProductService.java
@@ -4,7 +4,7 @@ package test.microstream.integrations.spring.boot.service;
  * #%L
  * microstream-integrations-spring-boot
  * %%
- * Copyright (C) 2019 - 2022 MicroStream Software
+ * Copyright (C) 2019 - 2023 MicroStream Software
  * %%
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
@@ -20,13 +20,13 @@ package test.microstream.integrations.spring.boot.service;
  * #L%
  */
 
-import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 import test.microstream.integrations.spring.boot.database.Products;
 import test.microstream.integrations.spring.boot.model.Product;
 
 import java.util.Collection;
 
-@Controller
+@Service
 public class ProductService {
 
 
