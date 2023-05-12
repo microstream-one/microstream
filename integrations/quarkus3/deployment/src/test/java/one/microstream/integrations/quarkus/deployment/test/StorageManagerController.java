62a886a49dc8521a5d5ab43342d56ac9b8d65f75
Rudy De Busscher <r.d.busscher@microstream.one>
2023-05-12 13:59:03 +0200

    Integration for Quarkus 3.x. Fixes #564

---
+++ integrations/quarkus3/deployment/src/test/java/one/microstream/integrations/quarkus/deployment/test/StorageManagerController.java
@@ -0,0 +1,44 @@
+package one.microstream.integrations.quarkus.deployment.test;
+
+/*-
+ * #%L
+ * MicroStream Quarkus 3 Extension - Deployment
+ * %%
+ * Copyright (C) 2019 - 2023 MicroStream Software
+ * %%
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ * 
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License, v. 2.0 are satisfied: GNU General Public License, version 2
+ * with the GNU Classpath Exception which is
+ * available at https://www.gnu.org/software/classpath/license.html.
+ * 
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ * #L%
+ */
+
+import one.microstream.storage.types.StorageManager;
+
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/test")
+@Produces(MediaType.TEXT_PLAIN)
+public class StorageManagerController
+{
+
+    @Inject
+    StorageManager storageManager;
+
+    @GET
+    public String testStorageManager()
+    {
+        return "storageManagerRoot=" + (storageManager.root() == null);
+    }
+}
