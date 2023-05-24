6d4cdc290f82220aa40427c8afad34aae4f47648
Zdenek Jonas <johny2000uwb@gmail.com>
2023-05-24 14:57:07 +0200

    change module names

--- codegen/wrapping/src/main/java/module-info.java
+++ codegen/wrapping/src/main/java/module-info.java
@@ -17,7 +17,7 @@
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  * #L%
  */
-module microstream.wrapping.codegen
+module microstream.codegen.wrapping
 {
 	exports one.microstream.wrapping.codegen;
 
